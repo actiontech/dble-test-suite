90bc8528a6ca0fb34bd551597d4ff44d675ee07d
zhaohongjie <jianhan513@163.com>
2018-04-11 09:35:04 +0800

    fix compare sql result bug

--- compare_result.sh
+++ compare_result.sh
@@ -2,12 +2,7 @@
 generate_simple_cmp_file () {
 	files=$(ls $1| egrep -v "*_pass.log|balance")
 
-	cmp_dir="cmp_result"
-    if [ -d "$cmp_dir" ]; then
-        rm -rf "$cmp_dir"
-    fi
-
-    mkdir "$cmp_dir"
+    cmp_dir=$2
 
     for s in $files; do
 #        echo "file: $s"
@@ -15,19 +10,24 @@ generate_simple_cmp_file () {
             mkdir "$cmp_dir/$s"
             subfiles=$(ls "$1/$s"| grep -E -v *_pass.log)
             for ss in $subfiles; do
+                echo "$1/$s/$ss"
+                echo "$cmp_dir/$s/$ss"
                 grep '===file:' "$1/$s/$ss" > "$cmp_dir/$s/$ss"
                 grep '===id:' "$1/$s/$ss" > "$cmp_dir/$s/$ss"
             done
 		else
+		    echo "$1/$s/$ss"
+            echo "$cmp_dir/$s/$ss"
             grep '===file:' "$1/$s" > "$cmp_dir/$s"
             grep '===id:' "$1/$s" > "$cmp_dir/$s"
         fi
     done
 }
 
-rm -rf cmp_result
-generate_simple_cmp_file std_result
-generate_simple_cmp_file logs/result
+rm -rf cmp_result && mkdir cmp_result
+rm -rf cmp_std_result && mkdir cmp_std_result
+generate_simple_cmp_file std_result cmp_std_result
+generate_simple_cmp_file logs/result cmp_result
 
 res=`diff -qr cmp_std_result cmp_result`
 if [ ${#res} -gt 0 ]; then
