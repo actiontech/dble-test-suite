dbe531cc64b3193ade7fbaf6472799a7bf6d7f64
FlyingMao <1309765581@qq.com>
2018-05-15 11:17:57 +0800

    add init for cases related to issues:85

--- sqls/select/reference_shard_noshard.sql
+++ sqls/select/reference_shard_noshard.sql
@@ -57,7 +57,7 @@ select * from a_test union all select * from a_manager union all select * from a
 select * from a_test union distinct select * from a_manager union distinct select * from a_order_no_shard
 (select name from a_test where pad=1 order by id limit 10) union all (select name from a_order_no_shard where pad=1 order by id limit 10) order by name
 (select name from a_test where pad=1 order by id limit 10) union distinct (select name from a_order_no_shard where pad=1 order by id limit 10) order by name
-(select * from a_test where pad=1) union (select * from a_order_no_shard where pad=1) order by id limit 10
+(select * from a_test where pad=1) union (select * from a_order_no_shard where pad=1) order by name limit 10
 (select name as sort_a from a_test where pad=1) union (select name from a_order_no_shard where pad=1) order by sort_a limit 10
 (select name as sort_a,pad from a_test where pad=1) union (select name,pad from a_order_no_shard where pad=1) order by sort_a,pad limit 10
 #
