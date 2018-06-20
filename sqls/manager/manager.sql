94a61b70c44a7d7d694024895802da563dd18819
zhaohongjie <jianhan513@163.com>
2018-06-20 18:57:41 +0800

    1,append some cases in schema.feature 2, get XMLUtil works more convinent 3, fix bugs in manager.sql test

--- sqls/manager/manager.sql
+++ sqls/manager/manager.sql
@@ -1,24 +1,38 @@
-drop table if exists a_test
-CREATE TABLE a_test(`id` int(10) unsigned NOT NULL,`t_id` int(10) unsigned NOT NULL DEFAULT '0',`name` char(120) NOT NULL DEFAULT '',`pad` int(11) NOT NULL,PRIMARY KEY (`id`),KEY `k_1` (`t_id`))DEFAULT CHARSET=UTF8
-insert into a_test values(1,1,'test中id为1',1),(2,2,'test_2',2),(3,3,'test中id为3',4),(4,4,'$test$4',3),(5,5,'test...5',1),(6,6,'test6',6)
-#port:9066
+#select
+select @@VERSION_COMMENT
+select @@SESSION.TX_READ_ONLY
+select @@max_allowed_packet
+
+#set
+set @a=20；
+SET SESSION sql_mode = 'TRADITIONAL';
+SET sql_mode = 'TRADITIONAL';
+
+#show
 show @@session
-show @@connection.sql
+#show @@cache
 show @@connection
+show @@connection.sql
 show @@backend
 show @@command
 show @@heartbeat
 show @@database
 show @@datanode
+show @@datanode where schema=mytest;
+show @@datanodes where schema=mytest;
+show @@datanodes where schema=mytest and table=a_test;
 show @@datasource
 show @@datasource.synstatus
+show @@datasource.syndetail where name="172.100.9.5"
+show @@datasource.cluster
 show @@processor
 show @@help
 show @@server
 show @@sysparam
+show @@sql
+show @@sql.high
 show @@threadpool
 show @@time.current
 show @@time.startup
 show @@version
-show @@datanodes where schema=mytest and table=a_test;
 show @@algorithm where schema=mytest and table=a_test;
