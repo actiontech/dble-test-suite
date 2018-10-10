8c9c6ef3c02d4065c75c0735fdfe17a061e2ccd9
zhaohongjie <jianhan513@163.com>
2018-10-10 16:53:28 +0800

    refactor multiQuery

--- drivers/c_mysql_api/multi-queries/multiQuery.sh
+++ drivers/c_mysql_api/multi-queries/multiQuery.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -e
 make clean
 make
 ./multiQuery.o > curr.output 2>&1
-`diff -wq curr.output multiQuery.output`
-let asExpect=`echo $?`
-echo $asExpect
-exit $asExpect
+echo "compare multiQuery's output with stand: diff -wy curr.output multiQuery.output"
+diff -wq curr.output multiQuery.output
+if [[ ${asExpect} -eq 0 ]]; then
+    echo "test result is same with std_result, case pass !"
+fi
 
