138c077caa72d95a5d40c22a3186305867e808ea
zhaohongjie <jianhan513@163.com>
2018-11-16 18:29:43 +0800

    mainly for adjusting env code to make dble_autotest_vm template

--- compose/install_zookeeper.sh
+++ compose/install_zookeeper.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+#this script is tested for zookeeper-3.4.9.tar.gz
+source_dir=$( dirname ${BASH_SOURCE[0]} )
+
 dble_install=("dble-1" "dble-2" "dble-3")
 rm -r /opt/zookeeper
-cd /init_assets && tar -zxf zookeeper-3.4.9.tar.gz
-mv /init_assets/zookeeper-3.4.9 /opt/zookeeper
+cd ${source_dir} && tar -zxf zookeeper-*.tar.gz
+zk_full_name=`ls ${source_dir}| egrep -w "zookeeper*" | grep -v ".tar.gz"`
+mv ${source_dir}/${zk_full_name} /opt/zookeeper
 mkdir -pv /opt/zookeeper/{data,logs}/
 echo " ">> /opt/zookeeper/conf/zoo.cfg
 sed -i "$ a tickTime=2000\ninitLimit=10\nsyncLimit=5 \nclientPort=2181" /opt/zookeeper/conf/zoo.cfg
