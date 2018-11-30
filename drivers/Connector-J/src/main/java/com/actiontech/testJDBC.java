a9a851768222cc9105895417e9e739da46c80a34
wjl1619 <401172143@qq.com>
2018-11-30 18:25:40 +0800

    add function to handle manager sqls

--- drivers/Connector-J/src/main/java/com/actiontech/testJDBC.java
+++ drivers/Connector-J/src/main/java/com/actiontech/testJDBC.java
@@ -32,6 +32,13 @@ public class testJDBC {
         Object client_password = cdble.get("client_password");
         String dbleuserPwd = client_password.toString();
         String dbleURL = "jdbc:mysql://10.186.60.61:7131/mytest";
+        //manager
+        Object manager_user = cdble.get("manager_user");
+        String dblemanageruserName = manager_user.toString();
+        Object manager_password = cdble.get("manager_password");
+        String dblemanageruserPwd = manager_password.toString();
+        String dblemanagerURL = "jdbc:mysql://10.186.60.61:7161/mytest";
+
         //获取mysql链接参数
         Object cfg_mysql = com.actiontech.yamlParser.getConfig(ylPath_value, "cfg_mysql");
         ObjectMapper oMappermysql = new ObjectMapper();
@@ -41,63 +48,96 @@ public class testJDBC {
         String msqluserName = user.toString();
         String mysqluserPwd = password.toString();
         String mysqlURL = "jdbc:mysql://10.186.60.61:7144/mytest";
-        //链接数据库
-        Connection dbleconn = null;
-        Connection mysqlconn = null;
-        dbleconn = conn.DBconn(dbleURL, dbleuserName, dbleuserPwd);
-        mysqlconn = conn.DBconn(mysqlURL, msqluserName, mysqluserPwd);
-        if (dbleconn != null && mysqlconn != null) {
-            Statement dblestmt = null;
-            Statement mysqlstmt = null;
-            try {
-                dblestmt = dbleconn.createStatement();
-            } catch (SQLException e) {
-                e.printStackTrace();
-                System.out.println("dble创建statement失败");
-                return;
-            }
-            try {
-                mysqlstmt = mysqlconn.createStatement();
-            } catch (SQLException e) {
-                e.printStackTrace();
-                System.out.println("mysql创建statement失败");
-                return;
-            }
-            com.actiontech.execandCompare.execandCompare(rtPath_value, sqPath_value, dblestmt, mysqlstmt);
-            //关闭statement链接
-            if (dblestmt != null) {
+        if (sqPath_value.contains("manager")) {
+            Connection dblemanagerconn = null;
+            dblemanagerconn = conn.DBconn(dblemanagerURL, dblemanageruserName, dblemanageruserPwd);
+            if (dblemanagerconn != null) {
+                Statement dblemanagerstmt = null;
                 try {
-                    dblestmt.close();
+                    dblemanagerstmt = dblemanagerconn.createStatement();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    System.out.println("dble创建statement失败");
+                    return;
+                }
+                com.actiontech.executeManager.execute(rtPath_value, sqPath_value, dblemanagerstmt);
+                //关闭statement链接
+                if (dblemanagerstmt != null) {
+                    try {
+                        dblemanagerstmt.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    dblemanagerstmt = null;
+                }
+            }
+            if (dblemanagerconn != null) {
+                try {
+                    dblemanagerconn.close();
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
-                dblestmt = null;
+                dblemanagerconn = null;
             }
-            if (mysqlstmt != null) {
+        } else {
+            Connection dbleconn = null;
+            Connection mysqlconn = null;
+            dbleconn = conn.DBconn(dbleURL, dbleuserName, dbleuserPwd);
+            mysqlconn = conn.DBconn(mysqlURL, msqluserName, mysqluserPwd);
+            if (dbleconn != null && mysqlconn != null) {
+                Statement dblestmt = null;
+                Statement mysqlstmt = null;
                 try {
-                    mysqlstmt.close();
+                    dblestmt = dbleconn.createStatement();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    System.out.println("dble创建statement失败");
+                    return;
+                }
+                try {
+                    mysqlstmt = mysqlconn.createStatement();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    System.out.println("mysql创建statement失败");
+                    return;
+                }
+                com.actiontech.execandCompare.execandCompare(rtPath_value, sqPath_value, dblestmt, mysqlstmt);
+                //关闭statement链接
+                if (dblestmt != null) {
+                    try {
+                        dblestmt.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    dblestmt = null;
+                }
+                if (mysqlstmt != null) {
+                    try {
+                        mysqlstmt.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    mysqlstmt = null;
+                }
+            }
+            //关闭connection链接
+            if (dbleconn != null) {
+                try {
+                    dbleconn.close();
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
-                mysqlstmt = null;
+                dbleconn = null;
+            }
+            if (mysqlconn != null) {
+                try {
+                    mysqlconn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                mysqlconn = null;
             }
         }
-        //关闭connection链接
-        if (dbleconn != null) {
-            try {
-                dbleconn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            dbleconn = null;
-        }
-        if (mysqlconn != null) {
-            try {
-                mysqlconn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            mysqlconn = null;
-        }
+
     }
 }
