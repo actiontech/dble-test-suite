401279689a2af5f14325e3229ef4cbe472ba0a49
FlyingMao <1309765581@qq.com>
2019-01-28 17:30:54 +0800

    sql_cover sharding

--- sqls/sqls_util/dev_dealed/cross_db.sql
+++ sqls/sqls_util/dev_dealed/cross_db.sql
@@ -2,21 +2,21 @@
 #!share_conn
 drop table if exists schema1.test1;
 drop table if exists schema2.test2;
-drop table if exists schema3.test2;
+drop table if exists schema3.test3;
 create table schema1.test1(id int);
 create table schema2.test2(id int);
-create table schema3.test2(id int);
+create table schema3.test3(id int);
 
 insert into schema1.test1 values(1);
 insert into schema2.test2 values(2);
-insert into schema3.test2 values(3);
+insert into schema3.test3 values(3);
 
 select * from schema1.test1;
 select * from schema2.test2;
-select * from schema3.test2;
+select * from schema3.test3;
 select * from schema1.test1 join schema2.test2;
-select * from schema2.test2 join schema3.test2;
-select * from schema1.test1 join schema2.test2 join schema3.test2;
+select * from schema2.test2 join schema3.test3;
+select * from schema1.test1 join schema2.test2 join schema3.test3;
 
 use schema1;
-select * from test1 join schema2.test2 join schema3.test2;
+select * from test1 join schema2.test2 join schema3.test3;
