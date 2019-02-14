aabdb4a299aa9f7a844b94d0092e3567809f963d
zhaohongjie <jianhan513@163.com>
2019-02-14 10:39:05 +0800

    fix manual error

--- compose/restart_env.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#restarting server for power off or any other reason, you can run this script to restart service for autotest
-base_dir=$( dirname ${BASH_SOURCE[0]} )
-
-sudo systemctl start docker.service
-cd ${base_dir} && docker-compose start
-bash resetReplication.sh
