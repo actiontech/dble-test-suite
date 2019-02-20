9c85b4c604f416590d932429d8daf17d735aa369
FlyingMao <1309765581@qq.com>
2019-02-20 17:29:27 +0800

    update result of issue:920

--- behave_dble/sqls/sqls_util/syntax/reserved_words.sql
+++ behave_dble/sqls/sqls_util/syntax/reserved_words.sql
@@ -983,7 +983,7 @@ create index `create` on `select`(`key`)
 show index from `select`
 drop view if exists `drop`
 show create view `drop`
-create or replace view `drop` as select id from `select`cat
+create or replace view `drop` as select id from `select`
 show create view `drop`/*allow_diff*/
 drop index `create` on `select`
 drop view if exists `drop`
