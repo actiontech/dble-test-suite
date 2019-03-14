de0dee90bdf990337d99208a2b1da439f61752b8
zhaohongjie <jianhan513@163.com>
2019-03-14 15:21:56 +0800

    1.append cases for global sequnce column

--- behave_dble/features/steps/step_sequence.py
+++ behave_dble/features/steps/step_sequence.py
@@ -1,4 +1,7 @@
 import os
+import threading
+
+import math
 
 from behave_dble.features.steps.lib.DBUtil import DBUtil
 from behave_dble.features.steps.lib.Node import get_node
@@ -13,86 +16,35 @@ from lib.generate_util import generate
 
 LOGGER = logging.getLogger('steps.sequence')
 
-@Then('test queries with table using global sequence')
-def step_impl(context):
-    text = eval(context.text)
-    tb = text.get("table")
-    sequnceHandlerType = text.get("sequnceHandlerType")
+def insertOneRow(context, table, rowsPerThread):
+    conn = get_dble_conn(context)
+    sql = "insert into {0} values('abc')".format(table)
+    context.logger.debug("execute query in subthread")
+    for i in range(rowsPerThread):
+        res, errMsg = conn.query(sql)
+        assert errMsg is None, "expect '{0}' success, but get: {1}".format(sql, errMsg)
+    conn.close()
 
-    gen = generate()
-    rand_str = gen.rand_string(20)
-    dble_conn = get_dble_conn(context)
+@Then('insert "{totalsRowsInsert:d}" rows to global sequence table "{table}" in "{threadNum:d}" thread')
+def step_impl(context, totalsRowsInsert, table, threadNum):
+    realThreadNum = min(totalsRowsInsert, threadNum)
+    rowsPerThread = totalsRowsInsert/threadNum
+    roundantRows = totalsRowsInsert%threadNum
 
-    #can not assign value to sequenceColumn, and can assgin value to columns without sequenceColumn
-    context.execute_steps(u"""
-    Then execute sql in "dble-1" in "user" mode
-        | user | passwd | conn   | toClose | sql                                                                    | expect            | db     |
-        | test | 111111 | conn_0 | False   | drop table if exists {0}                                               | success           | schema1 |
-        | test | 111111 | conn_0 | False   | create table {0}(id {1} primary key auto_increment, name varchar(20))  | success           | schema1 |
-        | test | 111111 | conn_0 | False   | insert into {0} values('{2}')                                          | success           | schema1 |
-        | test | 111111 | conn_0 | False   | insert into {0}(name) values('{2}')          | success | schema1 |
-        | test | 111111 | conn_0 | False   | insert into {0}(id,name) values(1, '{2}')    | In insert Syntax, you can't set value for Autoincrement column| schema1 |
-        | test | 111111 | conn_0 | False   | insert into {0} set id=1, name='{2}'         | In insert Syntax, you can't set value for Autoincrement column| schema1 |
-        | test | 111111 | conn_0 | False   | insert into {0} set name='{2}'               | success | schema1 |
-        | test | 111111 | conn_0 | False   | insert into {0} values('{2}'),('{2}'),('{2}')| success | schema1 |
-        | test | 111111 | conn_0 | False   | drop table if exists {0}                     | success | schema1 |
-        | test | 111111 | conn_0 | False   | create table {0}(id {1} , name varchar(20))  | success | schema1 |
-        | test | 111111 | conn_0 | False   | insert into {0} values('{2}')                | success | schema1 |
-        | test | 111111 | conn_0 | False   | insert into {0}(name) values('{2}')          | success | schema1 |
-        | test | 111111 | conn_0 | False   | drop table if exists {0}                     | success | schema1 |
-        | test | 111111 | conn_0 | False   | create table {0}(idd {1}, name varchar(20))  | success | schema1 |
-        | test | 111111 | conn_0 | False   | insert into {0} values('{2}')                | please make sure your table structure has primaryKey or incrementColumn | schema1 |
-        | test | 111111 | conn_0 | False   | insert into {0}(name) values('{2}')          | please make sure your table structure has primaryKey or incrementColumn | schema1 |
-        | test | 111111 | conn_0 | True    | drop table if exists {0}                     | success | schema1 |
-    """.format(tb, "bigint", rand_str))
+    threadsList = []
+    for k in range(realThreadNum):
+        if k < roundantRows:
+            rowsPerThread = rowsPerThread + 1
+        LOGGER.debug("rowsPerThread to insert : {0}".format(rowsPerThread))
+        thd_name = "insert_thd_{0}".format(k)
+        thd = threading.Thread(target=insertOneRow, name=thd_name, args=(context, table, rowsPerThread))
+        threadsList.append(thd)
 
-    #use int not bigint as global sequnce
-    rand_str = gen.rand_string(20)
-    context.execute_steps(u"""
-    Then execute sql in "dble-1" in "user" mode
-        | user | passwd | conn   | toClose | sql                                                                    | expect            | db     |
-        | test | 111111 | conn_0 | False   | drop table if exists {0}                                               | success           | schema1 |
-        | test | 111111 | conn_0 | False   | create table {0}(id {1} primary key auto_increment, name varchar(20))  | success           | schema1 |
-        | test | 111111 | conn_0 | False   | insert into {0} values('{2}')                      | Out of range value for column | schema1 |
-        | test | 111111 | conn_0 | True    | insert into {0} values('{2}'),('{2}'),('{2}')      | Out of range value for column | schema1 |
-    """.format(tb, "int", rand_str))
+    for i in range(realThreadNum):
+        threadsList[i].start()
 
-
-    # if "1" == sequnceHandlerType:
-    # prepare table for test
-    context.execute_steps(u"""
-    Then execute sql in "dble-1" in "user" mode
-        | user | passwd | conn   | toClose | sql                                                                    | expect   | db     |
-        | test | 111111 | conn_0 | False   | drop table if exists {0}                                               | success  | schema1 |
-        | test | 111111 | conn_0 | True    | create table {0}(id {1} primary key auto_increment, name varchar(20))  | success  | schema1 |
-    """.format(tb, "bigint", rand_str))
-
-    insertRow = 1000
-    val = "('{0}')".format(gen.rand_string(20))
-    for i in range(insertRow):
-        val = val + ",('{0}')".format(gen.rand_string(20))
-    sql = "insert into {0} values ('{1}')".format(tb, gen.rand_string(20))
-    dble_conn.query(sql)
-
-    # check for no repeatable id, attention: too little data is not able to capture the possible bug
-    sql = "select count(*) from {0} having count(*) > 1 group by id;".format(tb)
-    result, errMes = dble_conn.query(sql)
-    assert_that(result, is_(()))
-
-    # check for data distribution is reasonable
-    sql = "explain select * from {0}".format(tb)
-    result, errMes = dble_conn.query(sql)
-    if type(result) == tuple:
-        for i in range(len(result)):
-            sql = "/*!dble:dataNode={0}*/select count(*) from {1}".format(result[i][0], tb)
-            res, err = dble_conn.query(sql)
-            assert_that(int(res[0][0]), greater_than_or_equal_to(insertRow / 4))  # 4 is default shardings
-
-    # check for no id continuous
-    if "1" == sequnceHandlerType:
-        sql = "select max(id)-min(id) from {0};".format(tb)
-        result, errMes = dble_conn.query(sql)
-        assert_that(result[0][0], is_((insertRow)))
+    for i in range(realThreadNum):
+        threadsList[i].join()
 
 @Then('execute sqlFile to initialize sequence table')
 def step_impl(context):
