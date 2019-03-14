33fd44be95d81aec97fc7dad3fcf66347e312cc7
wjl1619 <401172143@qq.com>
2019-03-14 16:17:42 +0800

    for running fun and zk only

---
+++ behave_dble/compose/run_func_and_zk.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+bash resetReplication.sh && \
+cd ../ && \
+behave -Dreset=false -D dble_conf=template features/install_uninstall/install_dble.feature features/func_test && \
+behave -Dreinstall=true -Dis_cluster=true features/install_uninstall/single_dble_and_zk_cluster.feature features/install_uninstall/install_dble_cluster.feature features/cluster/ \
+features/install_uninstall/stop_dble_cluster.feature | tee -a fun_and_zk.log
