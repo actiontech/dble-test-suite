d60f3d0a135da5ccf3a4836d1b86efb3483ced40
wjl1619 <401172143@qq.com>
2019-03-15 14:16:09 +0800

    modified after code review

--- drivers/Connector-cpp/ext.h
+++ drivers/Connector-cpp/ext.h
@@ -12,17 +12,17 @@
 using namespace std;
 using namespace sql;
 
-int WriteLoadData(string filename);
-int createdir(const char *filepath);
+int writeLoadData(string fileName);
+int createDir(const char *filePath);
 //sql::Connection *conn(const char *hostName, const char *userName, const char *password);
 Connection* createConn(char *hostName, const char *userName, const char *password);
-int manager_exec(const char *sqlfile, const char *logpath, Connection *con);
-int client_exec(const char *sqlfile, const char *logpath, Connection *dblecon, Connection *mysqlcon);
-bool compareList(list<string> dblerslist, list<string> mysqlrslist, bool allow_diff_sequence);
-string convertList(list<string> lt);
+int manager_exec(const char *sqlFile, const char *logPath, Connection *con);
+int client_exec(const char *sqlFile, const char *logPath, Connection *dbleConn, Connection *mysqlConn);
+bool compareList(list<string> dbleResultSetList, list<string> mysqlResultSetList, bool allow_diff_sequence);
+string convertListToString(list<string> lt);
 list<string> exec_sql(Statement *stmt, string line);
 bool findSubstr(string str, string substr);
-Config YParse(string yamlpath);
-int RmFile(const char *filepath);
+Config YamlParse(string yamlPath);
+int removeFile(const char *filePath);
 
 #endif
