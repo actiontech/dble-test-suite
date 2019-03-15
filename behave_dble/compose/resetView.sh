6b6e591184dba3964e24dd085ad93c402e25f54a
wjl1619 <401172143@qq.com>
2019-03-15 17:26:44 +0800

    clean view

---
+++ behave_dble/compose/resetView.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+base_dir=$( dirname ${BASH_SOURCE[0]} )
+echo $1
+dble_install=$@
+count=${#dble_install[@]}
+for((i=0; i<count; i=i+1)); do
+    echo "reset views in ${dble_install[$i]} starting"
+    #remove viewConf
+    echo "remove viewConf in ${dble_install[$i]}"
+    docker exec ${dble_install[$i]} sh -c "rm -rf /opt/dble/viewConf"
+    echo "remove viewConf in ${dble_install[$i]} success"
+
+    #remove dble registered nodes in zk
+    echo "remove dble registered nodes ${dble_install[$i]}"
+    docker exec ${dble_install[$i]} sh -c "cd /opt/zookeeper/bin && sh zkCli.sh rmr /dble"
+    echo "remove dble in ${dble_install[$i]} success"
+
+    echo "reset views in ${dble_install[$i]} success"
+    echo "-----------------------------------------------------------"
+done
