2e6b2dee3af3b4288c4b8f01a2bbee53e9cbef02
zhaohongjie <jianhan513@163.com>
2019-03-22 13:51:15 +0800

    add run_func.sh to fix allure-report related problem

---
+++ behave_dble/run_func.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+behave -Dreset=false -D dble_conf=template -f allure_behave.formatter:AllureFormatter -o report1 features/install_uninstall/install_dble.feature features/func_test;test ${PIPESTATUS[0]} -eq 0
+if [ $? -ne 0 ]; then
+    echo "testCases failed, please check result detail at 'http://10.186.18.19:8082/index.html' !"
+    exit 1
+else
+    echo "testCases success, please check result detail at 'http://10.186.18.19:8082/index.html' !"
+fi
