bc4c1ac5c7ef34ea25706d49ac1daa84b4a4eace
yexiaoli88 <yexiaoli@actionsky.com>
2019-05-09 19:48:30 +0800

    driver related merge

--- drivers/Connector-cpp/run.sh
+++ drivers/Connector-cpp/run.sh
@@ -2,9 +2,12 @@
 
 need_compare=${1-"false"}
 
+rm -rf sql_logs
+
 cd src
 ./a.out "" "../conf/auto_dble_test.yaml" "driver_test_client.sql" "driver_test_manager.sql"
 
+mv sql_logs ../sql_logs
 cd ../
 if [ "$need_compare" = "-c" ]; then
     echo "comparing results..."
