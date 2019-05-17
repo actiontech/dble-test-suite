85350412accf39ffce70bd91c2670166a0be549d
yexiaoli <yexiaoli@actionsky.com>
2019-05-17 16:54:48 +0800

    move mysql initialize to mysql_init.sh

--- behave_dble/compose/docker-build-general/mysql_init.sh
+++ behave_dble/compose/docker-build-general/mysql_init.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
- mkdir -p /var/lib/mysql && touch /var/lib/mysql/auto.cnf && echo "server-uuid=$(uuidgen)" > /var/lib/mysql/auto.cnf
+/usr/local/mysql/bin/mysqld --initialize-insecure --basedir=/usr/local/mysql --user=mysql >/dev/null 2>&1
+mkdir -p /var/lib/mysql && touch /var/lib/mysql/auto.cnf && echo "server-uuid=$(uuidgen)" > /var/lib/mysql/auto.cnf
 /usr/local/mysql/bin/mysqld_safe --defaults-file=/etc/my.cnf --skip-grant-tables --user=mysql >/dev/null 2>&1 &
 sleep 30s && /usr/local/mysql/bin/mysql -uroot --skip-password -e "flush privileges;alter user root@'localhost' identified by '111111'"
