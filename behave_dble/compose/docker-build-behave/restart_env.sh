9e603de377746f975dcf2fe524db0689f4763d2d
yexiaoli88 <yexiaoli@actionsky.com>
2019-05-20 16:44:41 +0800

    Update Dockerfile

--- behave_dble/compose/docker-build-behave/restart_env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Copyright (C) 2016-2019 ActionTech.
-# License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher.
-#restarting server for power off or any other reason, you can run this script to restart service for autotest
-base_dir=$( dirname ${BASH_SOURCE[0]} )
-
-sudo systemctl start docker.service
-cd ${base_dir} && docker-compose start
-bash resetReplication.sh
