eff708d6e7fd6e1b521076edcde07c6c858a7d8b
yexiaoli88 <yexiaoli@actionsky.com>
2019-05-23 18:48:39 +0800

    update readme

--- behave_dble/compose/start_env.sh
+++ behave_dble/compose/start_env.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir /opt/behave/
+cd /opt/behave/ && git clone https://github.com/actiontech/dble-test-suite.git
+cd /opt/behave/dble-test-suite/behave_dble/compose/
 docker network create --gateway 172.100.9.253 --subnet 172.100.9.0/24 dble_test
 docker-compose -f docker-compose.yml up -d --force
 docker exec driver-test sh -c "/etc/init.d/ssh start"
