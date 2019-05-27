60a78a0c0222b3bf3a8893f3bb9fe3d748bc729f
wujinling <wujinling@actionsky.com>
2019-05-27 11:46:05 +0800

    change version

--- behave_dble/run_func.sh
+++ behave_dble/run_func.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright (C) 2016-2019 ActionTech.
-# License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher.
+# License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 behave -Dreset=false -D dble_conf=template -f allure_behave.formatter:AllureFormatter -o report1 features/install_uninstall/install_dble.feature features/func_test;test ${PIPESTATUS[0]} -eq 0
 if [ $? -ne 0 ]; then
     echo "testCases failed, please check result detail at 'http://10.186.18.19:8082/index.html' !"
