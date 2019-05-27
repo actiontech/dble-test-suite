574ecc5d0e6bc1404678a9214383038a2a9e1309
wujinling <wujinling@actionsky.com>
2019-05-27 11:35:29 +0800

    change version

--- behave_dble/features/steps/step_install.py
+++ behave_dble/features/steps/step_install.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2016-2019 ActionTech.
-# License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher.
+# License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 import os
 import re
 import time
