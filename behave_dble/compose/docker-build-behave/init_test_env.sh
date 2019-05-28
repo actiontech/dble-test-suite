802b74a8ba1d6cb0075369ea0c3f7d34d3a6d010
yexiaoli88 <yexiaoli@actionsky.com>
2019-05-28 18:05:01 +0800

    change ssh permission

--- behave_dble/compose/docker-build-behave/init_test_env.sh
+++ behave_dble/compose/docker-build-behave/init_test_env.sh
@@ -2,19 +2,19 @@
 #run this script for the first time init environment
 
 #ssh with no pwd
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub dble-1:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub dble-2:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub dble-3:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub mysql:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub mysql-master1:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub mysql-master2:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub driver-test:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@dble-1:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@dble-2:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@dble-3:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql-master1:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql-master2:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@driver-test:/root/.ssh/authorized_keys
 
 #init mysql passwd
 mysql_install=("mysql" "mysql-master1" "mysql-master2" "dble-1" "dble-2" "dble-3")
 count=${#mysql_install[@]}
 for((i=0; i<count; i=i+1)); do
-    ssh ${mysql_install[$i]}  "bash /usr/local/bin/mysql_init.sh"
+    ssh root@${mysql_install[$i]}  "bash /usr/local/bin/mysql_init.sh"
 done
 
 bash /docker-build/resetReplication.sh
