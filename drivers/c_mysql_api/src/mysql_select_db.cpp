dd95dd0297116285857a78474aa0d1322f206a20
wujinling <wujinling@actionsky.com>
2019-05-28 11:00:19 +0800

    change variable declaration

--- drivers/c_mysql_api/src/mysql_select_db.cpp
+++ drivers/c_mysql_api/src/mysql_select_db.cpp
@@ -10,13 +10,15 @@
 void case_mysql_select_db(MYSQL* conn){
 	printf("==>mysql_select_db test suites\n");
 
+    const char* sql="select count(*) from schema2.sharding_4_t2";
+
 	if(mysql_select_db(conn, "schema2")){
 		printf("mysql_select_db got err:%s\n", mysql_error(conn));
 		exit(1);
 	}else{
 		myquery(conn, "drop table if exists sharding_4_t2");
 		myquery(conn, "create table sharding_4_t2(id int)");
-        doQueryWithExpectInt(conn, "select count(*) from schema2.sharding_4_t2", 0);
+        doQueryWithExpectInt(conn,  const_cast<char*>(sql), 0);
         printf("    pass! create a table after mysql_select_db, then select from new_db.new_table success! \n");
 	}
 }
