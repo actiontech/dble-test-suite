bd8b587f028dbda203595f4c881396f2ef02b15b
zhaohongjie <jianhan513@163.com>
2019-08-23 10:12:30 +0800

    refactor dir structure

---
+++ drivers/jdbc-api-test/src/main/java/com/actiontech/dble/SSHCommandExecutor.java
@@ -0,0 +1,96 @@
+/* Copyright (C) 2016-2019 ActionTech.
+ * License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
+ */
+package com.actiontech.dble;
+
+import java.io.InputStream;
+import java.util.Vector;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+/**
+ * This class provide interface to execute command on remote Linux.
+ */
+
+public class SSHCommandExecutor {
+    private String ipAddress;
+
+    private String username;
+
+    private String password;
+
+    public static final int DEFAULT_SSH_PORT = 22;
+
+    private Vector<String> stdout;
+
+    public SSHCommandExecutor(final String ipAddress, final String username, final String password) {
+        this.ipAddress = ipAddress;
+        this.username = username;
+        this.password = password;
+        stdout = new Vector<String>();
+    }
+
+    public int execute(final String command) {
+        int returnCode = 0;
+        JSch jsch = new JSch();
+        MyUserInfo userInfo = new MyUserInfo();
+        try {
+            // Create and connect session.
+            Session session = jsch.getSession(username, ipAddress, DEFAULT_SSH_PORT);
+
+			java.util.Properties config = new java.util.Properties();
+			config.put("StrictHostKeyChecking", "no");
+			session.setConfig(config);
+			
+            session.setPassword(password);
+            session.setUserInfo(userInfo);
+            session.setTimeout(120000);
+            session.connect();
+
+            // Create and connect channel.
+            Channel channel = session.openChannel("exec");
+            ((ChannelExec)channel).setCommand(command);
+
+            channel.setInputStream(null);
+            ((ChannelExec)channel).setErrStream(System.err);
+
+            InputStream in=channel.getInputStream();
+            
+            Main.print_debug("ssh "+ipAddress+": " + command);
+            channel.connect();
+
+            byte[] tmp=new byte[1024];
+            while(true){
+              while(in.available()>0){
+                int i=in.read(tmp, 0, 1024);
+                if(i<0)break;
+                String outline=new String(tmp, 0, i);
+                stdout.add(outline);
+                Main.print_debug(outline);
+              }
+              if(channel.isClosed()){
+                if(in.available()>0) continue; 
+                Main.print_debug("exit-status: "+channel.getExitStatus());
+                break;
+              }
+              try{Thread.sleep(1000);}catch(Exception ee){}
+            }
+            channel.disconnect();
+            session.disconnect();
+            
+        }catch(JSchException e){
+        	e.printStackTrace();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return returnCode;
+    }
+
+    public Vector<String> getStandardOutput() {
+        return stdout;
+    }
+}
