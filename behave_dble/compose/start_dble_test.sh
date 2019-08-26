dd7e77fd6a0690e3e52eb0f28c7ee0aed83aa32e
zhaohongjie <jianhan513@163.com>
2019-08-26 14:22:47 +0800

    fix bug for install from local

--- behave_dble/compose/start_dble_test.sh
+++ behave_dble/compose/start_dble_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it behave bash -c "cd /init_assets/dble-test-suite/behave_dble;behave -Dreset=false -Dinstall_from_local=true features/install_uninstall/install_dble.feature;behave -Ddble_conf=sql_cover_global -Dinstall_from_local=true features/sql_cover/sql_global.feature;behave -Dinstall_from_local=true features/func_test/"
+docker exec -it behave bash -c "cd /init_assets/dble-test-suite/behave_dble;behave -Dreset=false -Dinstall_from_local=true -Dtest_config=auto_test_dble_release.yaml features/install_uninstall/install_dble.feature;behave -Ddble_conf=sql_cover_global -Dinstall_from_local=true -Dtest_config=auto_test_dble_release.yaml features/sql_cover/sql_global.feature;behave -Dinstall_from_local=true -Dtest_config=auto_test_dble_release.yaml features/func_test/"
