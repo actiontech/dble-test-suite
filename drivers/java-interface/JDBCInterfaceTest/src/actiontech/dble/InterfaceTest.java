64d4a09d0c5868602400754af5e8dcd7bee08d92
zhaohongjie <jianhan513@163.com>
2019-08-27 13:12:57 +0800

    append license info

--- drivers/java-interface/JDBCInterfaceTest/src/actiontech/dble/InterfaceTest.java
+++ drivers/java-interface/JDBCInterfaceTest/src/actiontech/dble/InterfaceTest.java
@@ -1,3 +1,6 @@
+/* Copyright (C) 2016-2019 ActionTech.
+ * License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
+ */
 package actiontech.dble;
 
 import java.sql.CallableStatement;
