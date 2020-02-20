dcd693d6db446e0ce1170a0dc39e8f30843e131a
zhaohongjie <zhaohongjie@actionsky.com>
2020-02-20 14:47:53 +0800

    integrate tasks of a job into one bash file

---
+++ drivers/Connector-cpp/do_run_connector_cpp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Copyright (C) 2016-2019 ActionTech.
+# License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
+need_compare=${1-"false"}
+
+rm -rf sql_logs
+
+cd src
+./a.out "" "conf/auto_dble_test.yaml" "driver_test_client.sql" "driver_test_manager.sql"
+
+mv sql_logs ../sql_logs
+cd ../
+if [ "$need_compare" = "-c" ]; then
+    echo "comparing results..."
+    bash compare_result.sh std_sql_logs sql_logs
+fi
