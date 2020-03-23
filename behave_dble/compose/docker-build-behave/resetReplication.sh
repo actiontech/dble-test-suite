7a94e796fefbd8e3774f5e1b2f13f46df8fc5312
wujinling <wujinling@actionsky.com>
2020-03-23 11:11:36 +0800

    for compatible with mysql version greater than 8.0

--- behave_dble/compose/docker-build-behave/resetReplication.sh
+++ behave_dble/compose/docker-build-behave/resetReplication.sh
@@ -20,7 +20,7 @@ echo "reset master mysql-master2"
 ssh root@${mysql_install[2]}  "/usr/local/mysql/bin/mysql -uroot -p111111 -h127.0.0.1 -P3306 -e \"reset master;\" "
 ssh root@${mysql_install[2]}  "sed -i -e '/log-bin=/d' -e '/binlog_format=/d' -e '/relay-log=/d' -e '/\[mysqld\]/a log-bin=mysql-bin \nbinlog_format=row \nrelay-log=mysql-relay-bin' /etc/my.cnf"
 ssh root@${mysql_install[2]}  "/usr/local/mysql/bin/mysql -uroot -p111111 -e \"create user if not exists 'repl'@'%' identified by '111111'\""
-ssh root@${mysql_install[2]}  "/usr/local/mysql/bin/mysql -uroot -p111111 -e \"grant replication slave on *.* to 'repl'@'%' identified by '111111'\""
+ssh root@${mysql_install[2]}  "/usr/local/mysql/bin/mysql -uroot -p111111 -e \"grant replication slave on *.* to 'repl'@'%'\""
 ssh root@${mysql_install[2]}  "/usr/local/mysql/support-files/mysql.server restart"
 
 sleep 60s
