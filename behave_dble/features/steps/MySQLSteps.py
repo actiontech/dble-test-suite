dfe005489c9bc59e96944f29fd73651ae00484ca
zhaohongjie <zhaohongjie@actionsky.com>
2020-04-04 12:57:04 +0800

    general log related refactor ok

--- behave_dble/features/steps/MySQLSteps.py
+++ behave_dble/features/steps/MySQLSteps.py
@@ -30,3 +30,44 @@ def stop_mysql(context, host_name):
 def start_mysql(context, host_name):
     mysql = MySQLFacotry.createMySQLObject(context, host_name)
     mysql.start()
+
+@Given('turn on general log in "{host_name}"')
+def step_impl(context,host_name):
+    mysql = MySQLFacotry.createMySQLObject(context, host_name)
+    mysql.turn_on_general_log()
+
+@Given('turn off general log in "{host_name}"')
+def turn_off_general_log(context,host_name):
+    mysql = MySQLFacotry.createMySQLObject(context, host_name)
+    mysql.turn_off_general_log()
+
+@Then('check general log in host "{host_name}" has not "{query}"')
+def step_impl(context,host_name, query):
+    mysql = MySQLFacotry.createMySQLObject(context, host_name)
+    mysql.check_query_in_general_log(query, expect_exist=False)
+
+@Then('check general log in host "{host_name}" has "{query}"')
+@Then('check general log in host "{host_name}" has "{query}" occured "{occur_times_expr}" times')
+def step_impl(context,host_name, query, occur_times_expr=None):
+    mysql = MySQLFacotry.createMySQLObject(context, host_name)
+    mysql.check_query_in_general_log(query, expect_exist=True, occur_times_expr=occur_times_expr)
+
+# @Given('execute sql in "{hostname}"')
+# @Then('execute sql in "{hostname}"')
+# @When('execute sql in "{hostname}" in "{user}" mode')
+# @Given('execute sql in "{hostname}" in "{user}" mode')
+# @Then('execute sql in "{hostname}" in "{user}" mode')
+# def execute_sql_in_host(context,hostname, user=""):
+#
+#     if len(user.strip()) == 0:
+#         node = get_node(context.mysqls, hostname)
+#         ip = node._ip
+#         port = node._mysql_port
+#     else:
+#         node = get_node(context.dbles, hostname)
+#         ip = node.ip
+#         if user == 'admin':
+#             port = node.manager_port
+#         else:
+#             port = node.client_port
+#     exec_sql(context, ip, port)
