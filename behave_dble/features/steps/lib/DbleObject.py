a450dc95da39c9ec5f112022497a47a56468a0d0
zhaohongjie <zhaohongjie@actionsky.com>
2020-04-04 12:57:04 +0800

    general log related refactor ok

---
+++ behave_dble/features/steps/lib/DbleObject.py
@@ -0,0 +1,5 @@
+# -*- coding=utf-8 -*-
+# Copyright (C) 2016-2020 ActionTech.
+# License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
+# @Time    : 2020/4/4 下午12:55
+# @Author  : irene-coming
