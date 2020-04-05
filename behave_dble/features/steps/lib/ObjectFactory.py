3fa5ee6533bafd429fa8121362bf0db9d4aa1e06
zhaohongjie <zhaohongjie@actionsky.com>
2020-04-05 19:14:09 +0800

    can run a little

--- behave_dble/features/steps/lib/ObjectFactory.py
+++ behave_dble/features/steps/lib/ObjectFactory.py
@@ -3,16 +3,22 @@
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 # @Time    : 2020/4/1 PM3:48
 # @Author  : irene-coming
-from .DbleObject import DbleObject
-from .MySQLObject import MySQLObject
-from .utils import get_node
+import logging
 
 
-def create_mysql_object(id):
-    mysql_meta = get_node(id)
-    return MySQLObject(mysql_meta)
+from DbleObject import DbleObject
+from MySQLObject import MySQLObject
+from utils import get_node
+logger = logging.getLogger('lib')
 
-def create_dble_object(id):
-    dble_meta = get_node(id)
-    return DbleObject(dble_meta)
+class ObjectFactory(object):
+    @classmethod
+    def create_mysql_object(self, id):
+        mysql_meta = get_node(id)
+        return MySQLObject(mysql_meta)
+
+    @classmethod
+    def create_dble_object(self,id):
+        dble_meta = get_node(id)
+        return DbleObject(dble_meta)
 
