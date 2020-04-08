aecf6e0e56a2f9f4b29c722a334673f59c4b421d
zhaohongjie <jianhan513@163.com>
2020-04-08 18:07:20 +0800

    debug refactor all is ok, skip autocommit case at present

--- behave_dble/features/steps/restart.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2016-2020 ActionTech.
-# License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
-# @Time    : 2018/5/18 PM4:58
-# @Author  : zhaohongjie@actionsky.com
-
-
-
