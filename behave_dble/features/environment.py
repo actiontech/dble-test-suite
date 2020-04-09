36acf2912a07f06e5fb33419487e7afca1824e2a
zhaohongjie <jianhan513@163.com>
2020-04-09 13:38:16 +0800

    debug ci fail for get_node get None

--- behave_dble/features/environment.py
+++ behave_dble/features/environment.py
@@ -2,17 +2,13 @@
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 import logging
 
+from steps.lib.DbleMeta import DbleMeta
+from steps.lib.MySQLMeta import MySQLMeta
 from steps.lib.MySQLObject import MySQLObject
-from steps.lib.utils import setup_logging ,load_yaml_config, init_meta,restore_sys_time,reset_repl, get_sftp
+from steps.lib.utils import setup_logging ,load_yaml_config, init_meta,restore_sys_time,reset_repl, get_sftp,get_ssh
+from steps.mysql_steps import turn_off_general_log, execute_sql_in_host, restart_mysql
 from steps.step_install import replace_config, set_dbles_log_level, restart_dbles, disable_cluster_config_in_node, \
     install_dble_in_all_nodes
-from steps.dble_steps import *
-from steps.mysql_steps import *
-from steps.step_function import *
-from steps.btrace_block import *
-from steps.step_createConn import *
-from steps.step_safety import *
-from steps.step_sequence import *
 
 logger = logging.getLogger('environment')
 
@@ -198,6 +194,7 @@ def get_case_tag_params(description, tag):
 
 def before_step(context, step):
     logger.debug(step.name)
+    logger.debug("debug1: {}".format(DbleMeta))
 
 def after_step(context, step):
     logger.info('{0}, status:{1}'.format(step.name, step.status))
