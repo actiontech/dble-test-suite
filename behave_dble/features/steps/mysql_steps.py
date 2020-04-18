fd7398e17f11870f766f92a4357d61a06f30531c
zhaohongjie <zhaohongjie@actionsky.com>
2020-04-18 21:30:22 +0800

    optimize schema_load_balance.feature spend too long to run

--- behave_dble/features/steps/mysql_steps.py
+++ behave_dble/features/steps/mysql_steps.py
@@ -3,8 +3,10 @@
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 # @Time    : 2020/4/1 PM1:35
 # @Author  : irene-coming
+import logging
 from threading import Thread
 
+from steps.lib.Flag import Flag
 from steps.lib.MySQLObject import MySQLObject
 from steps.lib.PostQueryCheck import PostQueryCheck
 from steps.lib.PreQueryPrepare import PreQueryPrepare
@@ -15,10 +17,7 @@ from behave import *
 global sql_threads
 sql_threads = []
 
-@Given('kill connection with query "{query}" in host "{host_name}"')
-def step_impl(context, query, host_name):
-    mysql = ObjectFactory.create_mysql_object(host_name)
-    mysql.kill_conn_with_query(query)
+logger = logging.getLogger('steps.mysql_steps')
 
 @Given('restart mysql in "{host_name}" with sed cmds to update mysql config')
 @Given('restart mysql in "{host_name}"')
@@ -69,19 +68,63 @@ def step_impl(context, host_name):
     for row in context.table:
         execute_sql_in_host(host_name, row.as_dict())
 
-def execute_sql_in_host(host_name, info_dic=None):
-    mysql = ObjectFactory.create_mysql_object(host_name)
-    query_meta = QueryMeta(info_dic, "mysql", mysql._mysql_meta)
+def execute_sql_in_host(host_name, info_dic, mode="mysql"):
+    if mode in ["admin", "user"]:#query to dble
+        obj = ObjectFactory.create_dble_object(host_name)
+        query_meta = QueryMeta(info_dic, mode, obj._dble_meta)
+    else:
+        obj = ObjectFactory.create_mysql_object(host_name)
+        query_meta = QueryMeta(info_dic, mode, obj._mysql_meta)
 
     pre_delegater = PreQueryPrepare(query_meta)
     pre_delegater.prepare()
 
-    res, err, time_cost = mysql.do_execute_query(query_meta)
+    res, err, time_cost = obj.do_execute_query(query_meta)
 
     post_delegater = PostQueryCheck(res, err, time_cost, query_meta)
     post_delegater.check_result()
 
-    return res,err
+    return res, err
+
+@Given('execute sql "{num}" times in "{host_name}" at concurrent')
+@Given('execute sql "{num}" times in "{host_name}" at concurrent {concur}')
+def step_impl(context, host_name, num, concur="100"):
+    row = context.table[0]
+    num = int(num)
+    info_dic = row.as_dict()
+    concur = min(int(concur), num)
+
+    tasks_per_thread = num/concur
+    mod_tasks = num%concur
+
+    def do_thread_tasks(host_name, info_dic, base_id, tasks_count, eflag):
+        my_dic = info_dic.copy()
+        my_dic["conn"] = "concurr_conn_{}".format(i)
+        last_count = tasks_count-1
+        sql_raw = my_dic["sql"]
+        for k in range(tasks_count):
+            if k==last_count:
+                my_dic["toClose"] = "true"
+            id = base_id+k
+            my_dic["sql"] = sql_raw.format(id)
+            # logger.debug("debug1, my_dic:{}, conn:{}".format(my_dic["sql"], my_dic["conn"]))
+            try:
+                execute_sql_in_host(host_name, my_dic, "user")
+            except Exception as e:
+                eflag.exception = e
+
+    for i in range(concur):
+        if i < mod_tasks:
+            tasks_count = tasks_per_thread + 1
+        else:
+            tasks_count = tasks_per_thread
+        base_id = i*tasks_per_thread
+        thd = Thread(target=do_thread_tasks, args=(host_name, info_dic, base_id, tasks_count, Flag))
+        thd.start()
+        thd.join()
+
+        if Flag.exception:
+            raise Flag.exception
 
 @Given('prepare a thread execute sql "{sql}" with "{conn_type}"')
 def step_impl(context, sql, conn_type=''):
@@ -124,3 +167,4 @@ def step_impl(context, host_name, conn_ids):
     assert len(conn_ids)>0, "no conns in '{}' to kill".format(conn_ids)
     mysql = ObjectFactory.create_mysql_object(host_name)
     mysql.kill_conns(conn_ids)
+
