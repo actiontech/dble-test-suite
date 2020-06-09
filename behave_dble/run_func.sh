359afa3b4667eebf2129b76036a29f49866c8ff0
wujinling <wujinling@actionsky.com>
2020-06-09 10:59:02 +0800

    modified run_func

--- behave_dble/run_func.sh
+++ behave_dble/run_func.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Copyright (C) 2016-2020 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
-behave -Dreset=false -D dble_conf=template features/install_uninstall/install_dble.feature features/func_test
+behave -Dreset=false -D dble_conf=template features/install_uninstall/install_dble.feature features/func_test/cfg_test/charset.feature features/func_test/cfg_test/dataNode_caseSensitive.feature features/func_test/cfg_test/lower_case_table_names.feature features/func_test/sharding_func_test features/func_test/ddl 
