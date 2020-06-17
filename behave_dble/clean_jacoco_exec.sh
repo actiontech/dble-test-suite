a72ae42b23d4bfdf4a9caf63b7669d746721aec1
wujinling <wujinling@actionsky.com>
2020-06-17 15:38:16 +0800

    add clean up for codecoverage

---
+++ behave_dble/clean_jacoco_exec.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Copyright (C) 2016-2020 ActionTech.
+# License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
+#for code coverage, clean up the dble_jacoco.exec for every new ci instance runs
+
+dble_install=("dble-1" "dble-2" "dble-3")
+count=${#dble_install[@]}
+for((i=0; i<count; i=i+1)); do
+	echo "clean up dble_jacoco.exec in ${dble_install[$i]}"
+	ssh root@${dble_install[$i]}  "echo >/opt/dble_jacoco.exec" \
+	&& sleep 2s
+done
