d8f9f567a847214496de7853d977e72e287bbae2
quexiuping <niannian@protonmail.com>
2020-06-29 11:23:45 +0800

    change

--- behave_dble/features/steps/step_reload.py
+++ behave_dble/features/steps/step_reload.py
@@ -1,5 +1,5 @@
 #coding= 'utf8'
-# Copyright (C) 2016-2019 ActionTech.
+# Copyright (C) 2016-2020 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 import MySQLdb
 import logging
@@ -7,97 +7,39 @@ import re
 from behave import *
 from hamcrest import *
 from lib.DBUtil import DBUtil
-from lib.Node import get_node, get_sftp
-from lib.XMLUtil import add_child_in_string, delete_child_node, get_xml_from_str, add_child_in_xml,change_root_node_properties
+from lib.utils import get_node, get_sftp
+from lib.XMLUtil import add_child_in_string, delete_child_node, get_xml_from_str, add_child_in_xml,change_root_node_properties,add_child_in_string_with_duplicate_name
 
 LOGGER = logging.getLogger('steps.reload')
 
 def get_dble_conn(context, default_db="schema1", node=None):
     if node is None:
-        node = get_node(context.dbles, "dble-1")
-    conn = DBUtil(node.ip, context.cfg_dble['client_user'],
-                         context.cfg_dble['client_password'], default_db, context.cfg_dble['client_port'],
+        node = get_node("dble-1")
+    conn = DBUtil(node.ip, node.client_user,
+                         node.client_password, default_db, node.client_port,
                          context)
     return conn
 
 def get_dble_connect(context,host_name,default_db="schema1"):
-    node = get_node(context.dbles, host_name)
-    conn = DBUtil(node.ip, context.cfg_dble['client_user'],
-                  context.cfg_dble['client_password'], default_db, context.cfg_dble['client_port'],
+    node = get_node(host_name)
+    conn = DBUtil(node.ip, node.client_user,
+                  node.client_password, default_db, node.client_port,
                   context)
     return conn
 
 def get_admin_conn(context, user="", passwd=""):
+    node = get_node("dble-1")
     if len(user.strip()) == 0:
-        user = context.cfg_dble['manager_user']
+        user = node.manager_user
     if len(passwd.strip()) == 0:
-        passwd = str(context.cfg_dble['manager_password'])
+        passwd = str(node.manager_password)
 
     conn = None
     try:
-        conn = DBUtil(context.cfg_dble['dble']['ip'], user, passwd, "", context.cfg_dble['manager_port'],context)
+        conn = DBUtil(node.ip, user, passwd, "", node.manager_port,context)
     except MySQLdb.Error, e:
         assert False, "create manager conn meets error:{0}".format(e.args)
     return conn
-
-@Then('execute admin cmd "{adminsql}"')
-@Then('execute admin cmd "{adminsql}" get the following output')
-@Then('execute admin cmd "{adminsql}" with user "{user}" passwd "{passwd}"')
-@Then('execute admin cmd "{adminsql}" with "{result}" result')
-def exec_admin_cmd(context, adminsql, user="", passwd="", result=""):
-    if len(user.strip()) == 0:
-        user = context.cfg_dble['manager_user']
-    if len(passwd.strip()) == 0:
-        passwd = str(context.cfg_dble['manager_password'])
-    if len(result.strip()) != 0:
-        adminsql = "{0} {1}".format(adminsql, getattr(context, result)[0][0])
-    if context.text: expect = context.text
-    else: expect = "success"
-
-    context.execute_steps(u"""
-    Then execute sql in "dble-1" in "admin" mode
-        | user    | passwd | conn    | toClose | sql      | expect   | db |
-        | {0}     | {1}    | new     | True    | {2}      | {3}      |    |
-    """.format(user, passwd, adminsql, expect))
-@Then('get resultset of admin cmd "{adminsql}" named "{rs_name}"')
-def step_impl(context, adminsql, rs_name):
-    manager_conn = get_admin_conn(context)
-    result, error = manager_conn.query(adminsql)
-    assert error is None, "execute adminsql {0}, get error:{1}".format(adminsql, error)
-    setattr(context, rs_name, result)
-
-@Then('get resultset of user cmd "{sql}" named "{rs_name}"')
-@Then('get resultset of user cmd "{sql}" named "{rs_name}" with connection "{conn_type}"')
-def step_impl(context, sql, rs_name, conn_type=''):
-    if hasattr(context, conn_type):
-        dble_conn = getattr(context, conn_type)
-        LOGGER.debug("get dble_conn: {0}".format(conn_type))
-    else:
-        dble_conn = get_dble_conn(context)
-    result, error = dble_conn.query(sql)
-    assert error is None, "execute usersql {0}, get error:{1}".format(sql, error)
-    setattr(context, rs_name, result)
-
-@Then('get result of user cmd "{sql}" named "{rs_name}" in dble "{host_name}"')
-def step_impl(context, sql, rs_name, host_name):
-    dble_conn = get_dble_connect(context, host_name)
-    result, error = dble_conn.query(sql)
-    assert error is None, "execute usersql {0}, get error:{1}".format(sql, error)
-    setattr(context, rs_name, result)
-
-@Then('get resultset of cmd "{sql}" named "{rs_name}" in mysql "{host_name}"')
-def step_impl(context, sql, rs_name, host_name):
-    node = get_node(context.mysqls, host_name)
-    ip = node._ip
-    port = node._mysql_port
-    user = "test"
-    passwd = "111111"
-    db = ""
-    conn = DBUtil(ip, user, passwd, db, port, context)
-    result,error = conn.query(sql)
-    assert error is None, "execute usersql {0}, get error:{1}".format(sql, error)
-    setattr(context,rs_name,result)
-
 
 @Then('removal result set "{rs_name}" contains "{key_word}" part')
 def step_impl(context, rs_name, key_word):
@@ -138,6 +80,13 @@ def add_xml_segment(context, kv_map_str, file):
     add_child_in_string(fullpath, kv_map, context.text)
     upload_and_replace_conf(context, file)
 
+@Given('add xml segment to node with attribute "{kv_map_str}" in "{file}" with duplicate name')
+def add_xml_segment(context, kv_map_str, file):
+    fullpath = get_abs_path(context, file)
+    kv_map = eval(kv_map_str)
+    add_child_in_string_with_duplicate_name(fullpath, kv_map, context.text)
+    upload_and_replace_conf(context, file)
+
 @Given('add attribute "{kv_map_str}" to rootnode in "{file}"')
 def add_attr_to_node(context, file, kv_map_str):
     fullpath = get_abs_path(context, file)
@@ -175,10 +124,12 @@ def delete_xml_segment(context):
 
 @When('Add some data in "{mapFile}"')
 def add_file(context,mapFile):
-    targetFile = "{0}/dble/conf/{1}".format(context.cfg_dble['install_dir'], mapFile)
+    node = get_node("dble-1")
+
+    targetFile = "{0}/dble/conf/{1}".format(node.install_dir, mapFile)
     text = str(context.text)
     cmd = "echo '{0}' > {1}".format(text, targetFile)
-    rc, sto, err = context.ssh_client.exec_command(cmd)
+    rc, sto, err = node.ssh_conn.exec_command(cmd)
     assert_that(err, is_(''), "expect no err, but err is: {0}".format(err))
 
 @When('Execute "{cmd}" on the managerment client and check system property with "{name}","{text}"')
@@ -197,24 +148,23 @@ def get_abs_path(context, file):
     return path
 
 def upload_and_replace_conf(context, filename, host='dble-1'):
+    node = get_node(host)
     local_file = get_abs_path(context, filename)
-    remote_file = "{0}/dble/conf/{1}".format(context.cfg_dble['install_dir'],filename)
-    if host == 'dble-1':
-        context.ssh_sftp.sftp_put(local_file, remote_file)
-    else:
-        sftpClient = get_sftp(context.dbles, host)
-        sftpClient.sftp_put(local_file, remote_file)
+    remote_file = "{0}/dble/conf/{1}".format(node.install_dir,filename)
+    node.sftp_conn.sftp_put(local_file, remote_file)
 
 def get_encrypt(context, string):
-    cmd = "source /etc/profile && sh {0}/dble/bin/encrypt.sh {1}".format(context.cfg_dble['install_dir'], string)
+    node= get_node("dble-1")
 
-    rc, sto, ste = context.ssh_client.exec_command(cmd)
+    cmd = "source /etc/profile && sh {0}/dble/bin/encrypt.sh {1}".format(node.install_dir, string)
+
+    rc, sto, ste = node.ssh_conn.exec_command(cmd)
     return sto.split('\n')[1]
 
 
 @Then('execute cmd "{cmd}" with "{result}" in mysql "{host}"')
 def step_impl(context, cmd, result, host):
-    node = get_node(context.mysqls, host)
+    node = get_node(host)
     ip = node._ip
     port = node._mysql_port
     user = "test"
@@ -228,9 +178,10 @@ def step_impl(context, cmd, result, host):
 
 @Given('get config xml version from template config and named as "{var_version}"')
 def step_impl(context, var_version):
-    cmd_server_version = "grep '<dble:server' {0}/dble/conf/server_template.xml| grep -o 'version=\".*\"' | grep -o '[0-9]*\.[0-9]*'".format(context.cfg_dble['install_dir'])
-    cmd_schema_version = "grep '<dble:schema' {0}/dble/conf/schema_template.xml| grep -o 'version=\".*\"' | grep -o '[0-9]*\.[0-9]*'".format(context.cfg_dble['install_dir'])
-    cmd_rule_version = "grep '<dble:rule' {0}/dble/conf/rule_template.xml| grep -o 'version=\".*\"' | grep -o '[0-9]*\.[0-9]*'".format(context.cfg_dble['install_dir'])
+    node = get_node("dble-1")
+    cmd_server_version = "grep '<dble:server' {0}/dble/conf/server_template.xml| grep -o 'version=\".*\"' | grep -o '[0-9]*\.[0-9]*'".format(node.install_dir)
+    cmd_schema_version = "grep '<dble:schema' {0}/dble/conf/schema_template.xml| grep -o 'version=\".*\"' | grep -o '[0-9]*\.[0-9]*'".format(node.install_dir)
+    cmd_rule_version = "grep '<dble:rule' {0}/dble/conf/rule_template.xml| grep -o 'version=\".*\"' | grep -o '[0-9]*\.[0-9]*'".format(node.install_dir)
 
     rc1, sto1, ste1 = context.ssh_client.exec_command(cmd_server_version)
     rc2, sto2, ste2 = context.ssh_client.exec_command(cmd_schema_version)
@@ -267,3 +218,32 @@ def step_impl(context,mysqlID,host):
     conn = DBUtil(ip, user, passwd, db, port, context)
     res,err = conn.query(sql)
     assert err is None, "kill connection is failed: {0}".format(err)
+
+@Then('record reloadTime of "{tbName}" from "{rs_name}" named "{rtName}"')
+def record_time(context, tbName, rs_name, rtName):
+    rs = getattr(context, rs_name)
+    count = 0
+    for rs_row in rs:
+        if rs_row[1]==tbName:
+            count = count+1
+            reload_time = rs_row[2].replace("/","-")
+            setattr(context,rtName,reload_time)
+            break
+    assert count != 0, "{0} is not found !".format(tbName)
+
+@Then('get resultset when reload time "{compare}" record time "{rtName}" named "{rs_name}"')
+def step(context, compare, rtName, rs_name):
+    rtn=getattr(context,rtName)
+    comp = ''
+    if compare == 'equal':
+        comp = '='
+    elif compare == 'lt':
+        comp = "<="
+    elif compare == 'gt':
+        comp = '>='
+    else: assert comp != '',"Comparison operator must from [{0}, {1}, {2}]".format("equal","lt","gt")
+    adminsql = "check full @@metadata where reload_time{0}'{1}'".format(comp,rtn)
+    manager_conn = get_admin_conn(context)
+    result, error = manager_conn.query(adminsql)
+    assert error is None, "execute adminsql {0}, get error:{1}".format(adminsql, error)
+    setattr(context, rs_name, result)
