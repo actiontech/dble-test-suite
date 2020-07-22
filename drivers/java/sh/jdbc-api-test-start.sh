36d92c337c69db6d73c28f323b70b8fd69a21739
guoaomen <guoaomen@actionsky.com>
2020-07-22 10:49:49 +0800

    fix: update packaging script

--- drivers/java/sh/jdbc-api-test-start.sh
+++ drivers/java/sh/jdbc-api-test-start.sh
@@ -1,1 +1,9 @@
-jar -cvfm ../jdbc_api_test.jar ../../jdbc-api-test/src
+echo '=======          copy sys.config         ======='
+cp ../../java-interface/JDBCInterfaceTest/sys.config ../
+echo '=======            package               ======='
+cd ../../jdbc-api-test && mvn -DskipTest clean install
+echo '=======    copy jdbc_api_test-1.0.jar    ======='
+cp target/jdbc_api_test-1.0.jar ../java
+echo '=======     start jdbc_api_test          ======='
+cd ../java && java -jar jdbc_api_test.jar
+echo '=======    START SUCCESS jdbc_api_test   ======='
