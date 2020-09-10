eb3ff24a15b5177a3737f74ce7380256e05e4ee3
wujinling <wujinling@actionsky.com>
2020-09-10 10:09:57 +0800

    fix ci fail 1636

--- drivers/Connector-J/run_connector_J.sh
+++ drivers/Connector-J/run_connector_J.sh
@@ -2,18 +2,19 @@
 # Copyright (C) 2016-2020 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 
+echo '=======                 restore the running environment              ======='
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd ${DIR}/../../behave_dble/compose/docker-build-behave && bash resetReplication.sh
 
-#restart dble
+echo '=======           restart dble with sql_cover_sharding               ======='
 cd ${DIR}/../../behave_dble && behave --stop -D dble_conf=sql_cover_sharding features/setup.feature
 
-#driver java test code is compiled in connector_j.jar
-cp /var/lib/go-agent/pipelines/connector_j.jar ${DIR}/target/
+echo '=======                       package                                ======='
+cd ${DIR} && mvn -DskipTest clean package assembly:assembly
 
-#do run driver test
-cd ${DIR} && bash do_run_connector_J.sh connector_j.jar -c
+echo '=======                        driver test                           ======='
+cd ${DIR}/ && bash do_run_connector_J.sh Jconnector-5.1.35-jar-with-dependencies.jar -c
 
-#save logs for ci artifacts
+echo '=======                   save logs for ci artifacts                 ======='
 scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
 mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
