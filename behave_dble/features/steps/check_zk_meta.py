5d0b450f84d1c7a9a705bd123c9164fa6f3399f6
mayingle <mayingle@actionsky.com>
2020-09-15 17:28:09 +0800

    add ZK metadata check for ZK cluster
    (cherry picked from commit 24692c5b695b5a897c6d73bf2500e0cfc1c8cb2a)

---
+++ behave_dble/features/steps/check_zk_meta.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2016-2020 ActionTech.
+# License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
+import MySQLdb
+import logging
+import json
+
+from behave import *
+from hamcrest import *
+from lib.DBUtil import *
+from lib.utils import get_node, get_ssh
+
+LOGGER = logging.getLogger('steps.check_zk_meta')
+
+@Then('get {path} on zkCli.sh for {info} on dble-1')
+def get_zk_meta_on_zkCli_sh(context, path, info):
+    # zk_method : zk cmd means : ls ,get, rmr, set, config ... etc...you can run help on zkCli.sh for more info
+    # /dble/cluster-1/conf/sharding
+    # info is function name such as:enum_func
+    # hostname is dble-1 ,dble-2,dble-3
+    cmd = "cd {0}/bin && ./zkCli.sh get {1}|grep '{2}'".format(context.cfg_zookeeper['home'], path, info)
+    # node = str(hostname)
+    cmd_ssh = get_ssh("dble-1")
+    rc, sto, ste = cmd_ssh.exec_command(cmd)
+    func_name = []
+    if any(sto):
+        outcome_dict = json.loads(sto)
+        LOGGER.debug("add debug to check the result of executing {0} is :sto:{1}".format(cmd, outcome_dict))
+        if outcome_dict.get('function'):
+            LOGGER.debug("{0} function is : {1} ".format(path ,outcome_dict.get('function')))
+            func_list = outcome_dict['function']
+            for func_obj in func_list:
+                if func_obj.get('name'):
+                    LOGGER.debug("{0} function is : {1} ".format(path, func_obj.get('name')))
+                    func_name.append(func_obj['name'])
+                else:
+                    LOGGER.debug("there are no keys named name in {0} ".format(path))
+            LOGGER.debug("we find function name are : {0}".format(func_name))
+            return func_name
+        else:
+            assert_that(False,"outcome dict is {0}".format(outcome_dict))
+
+    else:
+        LOGGER.debug("the sto is empty : {0}".format(sto))
+
