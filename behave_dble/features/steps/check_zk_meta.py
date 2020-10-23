7ae7d8a609cefd41fb21886076305cd4a6b01f09
mayingle <mayingle@actionsky.com>
2020-10-23 15:30:22 +0800

    finish repair codings

--- behave_dble/features/steps/check_zk_meta.py
+++ behave_dble/features/steps/check_zk_meta.py
@@ -2,6 +2,9 @@
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 import MySQLdb
 import logging
+
+from hamcrest.library.collection.isdict_containingkey import has_key
+
 import json
 
 from behave import *
@@ -22,23 +25,35 @@ def get_zk_meta_on_zkCli_sh(context, path, info):
     cmd_ssh = get_ssh("dble-1")
     rc, sto, ste = cmd_ssh.exec_command(cmd)
     func_name = []
-    if any(sto):
-        outcome_dict = json.loads(sto)
-        LOGGER.debug("add debug to check the result of executing {0} is :sto:{1}".format(cmd, outcome_dict))
-        if outcome_dict.get('function'):
-            LOGGER.debug("{0} function is : {1} ".format(path ,outcome_dict.get('function')))
-            func_list = outcome_dict['function']
-            for func_obj in func_list:
-                if func_obj.get('name'):
-                    LOGGER.debug("{0} function is : {1} ".format(path, func_obj.get('name')))
-                    func_name.append(func_obj['name'])
-                else:
-                    LOGGER.debug("there are no keys named name in {0} ".format(path))
-            LOGGER.debug("we find function name are : {0}".format(func_name))
-            return func_name
+    assert_that(sto, not_(empty()), "sto is not empty and it is : {0}".format(sto))
+    LOGGER.debug("the sto is empty : {0}".format(sto))
+    outcome_dict = json.loads(sto)
+    LOGGER.debug("add debug to check the result of executing {0} is :sto:{1}".format(cmd, outcome_dict))
+    assert_that(outcome_dict, has_key("function"), "we have a key named function and they are:{0}".format(outcome_dict))
+    LOGGER.debug("{0} function is : {1} ".format(path, outcome_dict.get('function')))
+    func_list = outcome_dict['function']
+    for func_obj in func_list:
+        if func_obj.get('name'):
+            LOGGER.debug("{0} function is : {1} ".format(path, func_obj.get('name')))
+            func_name.append(func_obj['name'])
         else:
-            assert_that(False,"outcome dict is {0}".format(outcome_dict))
+            LOGGER.debug("there are no keys named name in {0} ".format(path))
+    LOGGER.debug("we find function name are : {0}".format(func_name))
+    return func_name
 
-    else:
-        LOGGER.debug("the sto is empty : {0}".format(sto))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
