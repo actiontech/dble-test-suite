914094587539f846daf5929151240abadee56999
wjl1619 <44627441+wjl1619@users.noreply.github.com>
2020-11-19 10:23:44 +0800

    add restore view after scenarios，this request comes from dev (#185)
    * add restore view after scenarios
    
    * amend xa backgroup retry case and add network restore function
    
    * add skip tag for xa network case
    
    Co-authored-by: wujinling <wujinling@actionsky.com>

--- behave_dble/features/steps/RestoreEnvObject.py
+++ behave_dble/features/steps/RestoreEnvObject.py
@@ -9,8 +9,12 @@ from lib import utils
 
 from steps.lib.ObjectFactory import ObjectFactory
 from steps.mysql_steps import execute_sql_in_host
+# from steps.lib.utils import get_ssh
+from lib.utils import get_sftp, get_ssh,get_node
 
 import re
+
+from hamcrest import *
 
 logger = logging.getLogger('environment.after_scenario')
 class RestoreEnvObject(object):
@@ -24,6 +28,47 @@ class RestoreEnvObject(object):
         # if "aft_reset_replication" in self._scenario.tags:
         #     utils.reset_repl()
         #
+        if "restore_network" in self._scenario.tags:
+            params_dic = self.get_tag_params("{'restore_network'")
+            logger.debug("params_dic is: {0}".format(params_dic))
+            if params_dic:
+                paras = params_dic["restore_network"].split(",")
+                # paras = paras.split(",")
+            else:
+                paras = ""
+
+
+            logger.debug("try to restore_network: {0}".format(paras))
+            for host_name in paras:
+                logger.debug("the value of host_name is: {0}".format(host_name))
+                cmd = "iptables -F"
+                ssh = get_ssh(host_name)
+                rc, stdout, stderr = ssh.exec_command(cmd)
+                assert_that(len(stderr) == 0, "restore network with command:{1}, got err:{0}".format(stderr, cmd))
+
+        if "restore_view" in self._scenario.tags:
+            params_dic = self.get_tag_params("{'restore_view'")
+            if params_dic:
+                paras = params_dic["restore_view"]
+            else:
+                paras = {}
+
+            for host_name, mysql_vars in paras.items():
+                if host_name.find('dble')!=-1:
+                    mode="user"
+                else:
+                    mode = "mysql"
+                for k, v in mysql_vars.items():
+                    list_value = filter(lambda x: x, v.split(","))
+                    view_value=""
+                    for value in list_value:
+                        view_value=view_value + "{0}.{1},".format(k, value)
+                    query = "drop view if exists " + view_value
+
+                sql = query[:-1]#delete the last ','
+                # logger.debug("the sql is: {0}".format(sql))
+                execute_sql_in_host(host_name, {"sql": sql}, mode)
+
         if "restore_mysql_service" in self._scenario.tags:
             params_dic = self.get_tag_params("{'restore_mysql_service'")
 
@@ -36,11 +81,9 @@ class RestoreEnvObject(object):
             for host_name, mysql_vars in paras.items():
                 logger.debug("the value of host_name is: {0}".format(host_name))
                 for k, v in mysql_vars.items():
-                    logger.debug("the value of v is: {0}".format(v))
                     if v:
                         mysql = ObjectFactory.create_mysql_object(host_name)
                         mysql.start()
-
 
         if "restore_global_setting" in self._scenario.tags:
             params_dic = self.get_tag_params("{'restore_global_setting'")
@@ -59,6 +102,7 @@ class RestoreEnvObject(object):
 
                 sql = query[:-1]#delete the last ','
                 execute_sql_in_host(mysql, {"sql": sql})
+
         if "restore_mysql_config" in self._scenario.tags:
             params_dic = self.get_tag_params("{'restore_mysql_config'")
 
