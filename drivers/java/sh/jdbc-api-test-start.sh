c319fd814dfc3305e906d2168dd133277d1d6101
wjl1619 <44627441+wjl1619@users.noreply.github.com>
2020-11-30 13:15:09 +0800

    change capClientFoundRows to true for jdbc (#213)
    Co-authored-by: wujinling <wujinling@actionsky.com>

--- drivers/java/sh/jdbc-api-test-start.sh
+++ drivers/java/sh/jdbc-api-test-start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Copyright (C) 2016-2020 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
-
+echo '=======           modify  capClientFoundRows=true and then restart dble                 ======='
+ssh root@dble-1 "sed -i '\$a -DcapClientFoundRows=true' /opt/dble/conf/bootstrap.cnf && /opt/dble/bin/dble restart"
 echo '=======          copy sys.config         ======='
 cp ../../java-interface/JDBCInterfaceTest/sys.config ../
 echo '=======            package               ======='
@@ -11,3 +12,5 @@ cp target/jdbc_api_test-1.0-jar-with-dependencies.jar ../java
 echo '=======     start jdbc_api_test          ======='
 cd ../java && java -jar jdbc_api_test-1.0-jar-with-dependencies.jar
 echo '=======    START SUCCESS jdbc_api_test   ======='
+echo '=======    restore capClientFoundRows to default   ======='
+ssh root@dble-1 "sed -i -e '/capClientFoundRows/d' /opt/dble/conf/bootstrap.cnf && /opt/dble/bin/dble restart"
