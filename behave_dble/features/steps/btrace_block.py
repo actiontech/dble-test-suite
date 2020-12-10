4d96df571c11c89054df7d20f1db3f23174a27d8
quexiuping <quexiuping@actionsky.com>
2020-12-10 13:29:50 +0800

    change path about on user mode query "at background"

--- behave_dble/features/steps/btrace_block.py
+++ behave_dble/features/steps/btrace_block.py
@@ -73,7 +73,7 @@ def step_impl(context, host):
     time.sleep(5)
     for row in context.table:
         query_meta = QueryMeta(row.as_dict(), "user", node)
-        cmd = u"nohup mysql -u{} -p{} -P{} -c -D{} -e'{}' >/tmp/dble_query.log 2>&1 &".format(query_meta.user,query_meta.passwd,query_meta.port,query_meta.db,query_meta.sql)
+        cmd = u"nohup mysql -u{} -p{} -P{} -c -D{} -e'{}' >/tmp/dble_user_query.log 2>&1 &".format(query_meta.user,query_meta.passwd,query_meta.port,query_meta.db,query_meta.sql)
         rc, sto, ste = sshClient.exec_command(cmd)
         assert len(ste)==0, "impossible err occur"
 
