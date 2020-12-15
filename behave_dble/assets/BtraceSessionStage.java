276cc56d1afee84977bc478b24e6a2af06355642
wangjuan-action <75239673+wangjuan-action@users.noreply.github.com>
2020-12-15 10:40:31 +0800

    add about DBLE0REQ-726 and DBLE0REQ-12 (#229)
    * add about DBLE0REQ-726 and DBLE0REQ-12
    
    * update about DBLE0REQ-726 and DBLE0REQ-12
    
    * add about DBLE0REQ-726 and DBLE0REQ-12
    
    * add about DBLE0REQ-726 and DBLE0REQ-12
    
    * add Copyright and license
    
    Co-authored-by: snowpiaojuan <hbxhjwj@yeah.net>

---
+++ behave_dble/assets/BtraceSessionStage.java
@@ -0,0 +1,198 @@
+package com.actiontech.dble.btrace.script;
+
+import com.sun.btrace.BTraceUtils;
+import com.sun.btrace.annotations.BTrace;
+import com.sun.btrace.annotations.OnMethod;
+import com.sun.btrace.annotations.ProbeClassName;
+import com.sun.btrace.annotations.ProbeMethodName;
+import static com.sun.btrace.BTraceUtils.println;
+import static com.sun.btrace.BTraceUtils.str;
+
+import com.sun.btrace.annotations.Kind;
+import com.sun.btrace.annotations.Location;
+
+
+@BTrace(unsafe = true)
+public final class BtraceSessionStage {
+
+    private BtraceSessionStage() {
+
+    }
+
+    @OnMethod(
+            clazz = "com.actiontech.dble.server.NonBlockingSession",
+            method = "setRequestTime",
+            location = @Location(Kind.RETURN)
+    )
+    public static void setRequestTime(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
+        long startTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + startTime + "], start get into setRequestTime " );
+        BTraceUtils.println("------- start get into setRequestTime -------");
+        BTraceUtils.println();
+        Thread.sleep(100L);
+        long endTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + endTime + "], end get into setRequestTime " );
+        BTraceUtils.println();
+    }
+
+    @OnMethod(
+            clazz = "com.actiontech.dble.server.NonBlockingSession",
+            method = "startProcess",
+            location = @Location(Kind.RETURN)
+    )
+    public static void startProcess(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
+        long startTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + startTime + "], start get into startProcess " );
+        BTraceUtils.println("------- start get into startProcess -------");
+        BTraceUtils.println();
+        Thread.sleep(100L);
+        long endTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + endTime + "], end get into startProcess " );
+        BTraceUtils.println();
+    }
+
+    @OnMethod(
+            clazz = "com.actiontech.dble.server.NonBlockingSession",
+            method = "endParse",
+            location = @Location(Kind.RETURN)
+    )
+    public static void endParse(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
+        long startTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + startTime + "], start get into endParse " );
+        BTraceUtils.println("------- start get into endParse -------");
+        BTraceUtils.println();
+        Thread.sleep(100L);
+        long endTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + endTime + "], end get into endParse " );
+        BTraceUtils.println();
+    }
+
+    @OnMethod(
+            clazz = "com.actiontech.dble.server.NonBlockingSession",
+            method = "endRoute",
+            location = @Location(Kind.RETURN)
+    )
+    public static void endRoute(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
+        long startTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + startTime + "], start get into endRoute " );
+        BTraceUtils.println("------- start get into endRoute -------");
+        BTraceUtils.println();
+        Thread.sleep(100L);
+        long endTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + endTime + "], end get into endRoute " );
+        BTraceUtils.println();
+    }
+
+    @OnMethod(
+            clazz = "com.actiontech.dble.server.NonBlockingSession",
+            method = "setPreExecuteEnd",
+            location = @Location(Kind.RETURN)
+    )
+    public static void setPreExecuteEnd(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
+        long startTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + startTime + "], start get into setPreExecuteEnd " );
+        BTraceUtils.println("------- start get into setPreExecuteEnd -------");
+        BTraceUtils.println();
+        Thread.sleep(100L);
+        long endTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + endTime + "], end get into setPreExecuteEnd " );
+        BTraceUtils.println();
+    }
+
+    @OnMethod(
+            clazz = "com.actiontech.dble.server.NonBlockingSession",
+            method = "setBackendResponseTime",
+            location = @Location(Kind.RETURN)
+    )
+    public static void setBackendResponseTime(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
+        long startTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + startTime + "], start get into setBackendResponseTime " );
+        BTraceUtils.println("------- start get into setBackendResponseTime -------");
+        BTraceUtils.println();
+        Thread.sleep(100L);
+        long endTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + endTime + "], end get into setBackendResponseTime " );
+        BTraceUtils.println();
+    }
+
+    @OnMethod(
+            clazz = "com.actiontech.dble.server.NonBlockingSession",
+            method = "setResponseTime",
+            location = @Location(Kind.RETURN)
+    )
+    public static void setResponseTime(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
+        long startTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + startTime + "], start get into setResponseTime " );
+        BTraceUtils.println("------- start get into setResponseTime -------");
+        BTraceUtils.println();
+        Thread.sleep(100L);
+        long endTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + endTime + "], end get into setResponseTime " );
+        BTraceUtils.println();
+    }
+
+    @OnMethod(
+            clazz = "com.actiontech.dble.server.NonBlockingSession",
+            method = "setStageFinished",
+            location = @Location(Kind.RETURN)
+    )
+    public static void setStageFinished(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
+        long startTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + startTime + "], start get into setStageFinished " );
+        BTraceUtils.println("------- start get into setStageFinished -------");
+        BTraceUtils.println();
+        Thread.sleep(100L);
+        long endTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + endTime + "], end get into setStageFinished " );
+        BTraceUtils.println();
+    }
+
+    @OnMethod(
+            clazz = "com.actiontech.dble.server.NonBlockingSession",
+            method = "setBackendResponseEndTime",
+            location = @Location(Kind.RETURN)
+    )
+    public static void setBackendResponseEndTime(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
+        long startTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + startTime + "], start get into setBackendResponseEndTime " );
+        BTraceUtils.println("------- start get into setBackendResponseEndTime -------");
+        BTraceUtils.println();
+        Thread.sleep(100L);
+        long endTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + endTime + "], end get into setBackendResponseEndTime " );
+        BTraceUtils.println();
+    }
+
+    @OnMethod(
+            clazz = "com.actiontech.dble.server.NonBlockingSession",
+            method = "setBeginCommitTime",
+            location = @Location(Kind.RETURN)
+    )
+    public static void setBeginCommitTime(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
+        long startTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + startTime + "], start get into setBeginCommitTime " );
+        BTraceUtils.println("------- start get into setBeginCommitTime -------");
+        BTraceUtils.println();
+        Thread.sleep(100L);
+        long endTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + endTime + "], end get into setBeginCommitTime " );
+        BTraceUtils.println();
+    }
+
+    @OnMethod(
+            clazz = "com.actiontech.dble.server.NonBlockingSession",
+            method = "setHandlerEnd",
+            location = @Location(Kind.RETURN)
+    )
+    public static void setHandlerEnd(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
+        long startTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + startTime + "], start get into setHandlerEnd " );
+        BTraceUtils.println("------- start get into setHandlerEnd -------");
+        BTraceUtils.println();
+        Thread.sleep(100L);
+        long endTime = System.currentTimeMillis();
+        BTraceUtils.println("time[" + endTime + "], end get into setHandlerEnd " );
+        BTraceUtils.println();
+    }
+
+}
---
+++ behave_dble/features/steps/step_parameter.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-2020 ActionTech.
+# License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
+# @Time    : 2020/12/3
+# @Author  : wangjuan
+import logging
+from behave import Then, Given
+from steps.mysql_steps import execute_sql_in_host
+
+logger = logging.getLogger('steps.step_parameter')
+
+@Then('execute the sql in "{host_name}" in "{mode_name}" mode by parameter from resultset "{rs_name}"')
+@Given('execute the sql in "{host_name}" in "{mode_name}" mode by parameter from resultset "{rs_name}"')
+@Given('execute the sql in "{host_name}" in "{mode_name}" mode by parameter from resultset "{rs_name}" and save resultset in "{result_key}"')
+@Then('execute the sql in "{host_name}" in "{mode_name}" mode by parameter from resultset "{rs_name}" and save resultset in "{result_key}"')
+def step_impl(context, host_name, mode_name,rs_name, result_key=None):
+    param_value = getattr(context, rs_name)
+    assert param_value, "expect parameter not found in {0}".format(rs_name)
+
+    context.logger.debug("the parameter value is {0} ".format(param_value))
+
+    info_dict = context.table[0].as_dict()
+    info_dict['sql'] = info_dict['sql'].format(param_value)
+    info_dict['expect'] = info_dict['expect'].format(param_value)
+    context.logger.debug("the sql value is {0} ".format(info_dict['sql']))
+
+    res, _ = execute_sql_in_host(host_name, info_dict, mode_name)
+
+    if result_key is not None:
+        setattr(context, result_key, res)
+        context.logger.debug("the resultset {0}] is {1}".format(result_key, getattr(context, result_key)))
