f042f94d318ecff488b6f5cbaefea68ba27809d3
wjl1619 <44627441+wjl1619@users.noreply.github.com>
2021-01-04 13:25:52 +0800

    restore master compose dir (#253)
    Co-authored-by: wujinling <wujinling@actionsky.com>

---
+++ behave_dble/compose/start_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir /opt/behave/
+cd /opt/behave/ && git clone https://github.com/actiontech/dble-test-suite.git
+cd /opt/behave/dble-test-suite/behave_dble/compose/
+docker network create --gateway 172.100.9.253 --subnet 172.100.9.0/24 dble_test
+docker-compose -f docker-compose.yml up -d --force
+docker exec driver-test sh -c "/etc/init.d/ssh start"
+docker exec -it behave bash "/init_assets/dble-test-suite/behave_dble/compose/docker-build-behave/init_test_env.sh"
+
+/bin/bash
---
+++ behave_dble/compose/update_dble_version.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright (C) 2016-2020 ActionTech.
+# License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
+set -e;
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+#version like 2.19.09.0 or 9.9.9.9
+version=${1-"9.9.9.9"}
+old_dble_exists=0
+
+if [ -x "/opt/dble/bin/dble" ]; then
+    old_dble_exists=1
+    conf_path="dble_`date '+%Y-%m-%d_%H:%M:%S'`"
+
+#   stop running dble
+    /opt/dble/bin/dble stop
+
+#   mv dble conf for reuse
+    mv /opt/dble/conf /tmp/${conf_path}
+
+    rm -rf /opt/dble
+fi
+rm -rf /opt/actiontech-dble.tar.gz
+cd /opt && wget ftp://ftp:ftp@10.186.18.20/actiontech-dble/qa/${version}/actiontech-dble.tar.gz
+tar -zxf /opt/actiontech-dble.tar.gz -C /opt
+
+if [ ${old_dble_exists} -eq 1 ]; then
+# reuse old dble conf if exists and start dble
+  rm -rf /opt/dble/conf \
+  && mv /tmp/${conf_path} /opt/dble/conf \
+  && /opt/dble/bin/dble start
+else
+  echo "dble is first installed!!!"
+fi
