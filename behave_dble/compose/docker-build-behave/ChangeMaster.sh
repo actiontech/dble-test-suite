3b4bc0f85edd17baa832438e1b900fab4e51db9d
wujinling <wujinling@actionsky.com>
2021-01-04 11:33:24 +0800

    restore master compose dir

---
+++ behave_dble/compose/docker-build-behave/ChangeMaster.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+base_dir=$( dirname ${BASH_SOURCE[0]} )
+echo ${base_dir}
+echo "master is ${1},slaves are ${2} and ${3}"
+master_ip=`ssh root@${1} "hostname -i"`
+echo "master ip is ${master_ip}"
+
+
+echo "==================  reset master ${1}  =================="
+ssh root@${1}  "/usr/local/mysql/bin/mysql -uroot -p111111 -h127.0.0.1 -P3306 -e \"reset master;stop slave;reset slave all;\" "
+ssh root@${1}  "sed -i -e '/log-bin=/d' -e '/binlog_format=/d' -e '/relay-log=/d' -e '/\[mysqld\]/a log-bin=mysql-bin \nbinlog_format=row \nrelay-log=mysql-relay-bin' /etc/my.cnf"
+ssh root@${1}  "/usr/local/mysql/bin/mysql -uroot -p111111 -e \"create user if not exists 'repl'@'%' identified by '111111';\""
+ssh root@${1}  "/usr/local/mysql/bin/mysql -uroot -p111111 -e \"grant replication slave on *.* to 'repl'@'%'\""
+ssh root@${1}  "/usr/local/mysql/support-files/mysql.server restart"
+
+sleep 60s
+
+echo "==================  reset slave ${2}  =================="
+ssh root@${2}  "/usr/local/mysql/bin/mysql -uroot -p111111 -h127.0.0.1 -P3306 -e \"reset master;stop slave;reset slave all;set global gtid_purged='';\" "
+ssh root@${2}  "/usr/local/mysql/bin/mysql -uroot -p111111 -e \"change master to master_host='${master_ip}', master_user='repl', master_password='111111', master_auto_position=1;\""
+ssh root@${2}  "/usr/local/mysql/bin/mysql -uroot -p111111 -h127.0.0.1 -P3306 -e \"start slave;\" "
+
+echo "==================  reset slave ${3}  =================="
+ssh root@${3}  "/usr/local/mysql/bin/mysql -uroot -p111111 -h127.0.0.1 -P3306 -e \"reset master;stop slave;reset slave all;set global gtid_purged='';\" "
+ssh root@${3}  "/usr/local/mysql/bin/mysql -uroot -p111111 -e \"change master to master_host='${master_ip}', master_user='repl', master_password='111111', master_auto_position=1;\""
+ssh root@${3}  "/usr/local/mysql/bin/mysql -uroot -p111111 -h127.0.0.1 -P3306 -e \"start slave;\" "
---
+++ behave_dble/compose/docker-build-behave/init_test_env.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#run this script for the first time init environment
+base_dir=$( dirname ${BASH_SOURCE[0]} )
+echo ${base_dir}
+
+#ssh with no pwd
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@dble-1:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@dble-2:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@dble-3:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql-master1:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql-master2:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@driver-test:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql8-master1:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql8-master2:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql8-slave1:/root/.ssh/authorized_keys
+sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql8-slave2:/root/.ssh/authorized_keys
+
+#init mysql passwd
+mysql_install=("mysql" "mysql-master1" "mysql-master2" "dble-1" "dble-2" "dble-3" "mysql8-master1" "mysql8-master2" "mysql8-slave1" "mysql8-slave2")
+count=${#mysql_install[@]}
+for((i=0; i<count; i=i+1)); do
+    ssh root@${mysql_install[$i]}  "bash /usr/local/bin/mysql_init.sh"
+done
+
+bash ${base_dir}/resetReplication.sh
