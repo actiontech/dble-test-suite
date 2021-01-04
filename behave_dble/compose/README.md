2ca8d0046f2dad290fbdc7d986ab2f8571b739aa
wjl1619 <44627441+wjl1619@users.noreply.github.com>
2021-01-04 13:25:52 +0800

    restore master compose dir (#253)
    Co-authored-by: wujinling <wujinling@actionsky.com>

---
+++ behave_dble/compose/README.md
@@ -0,0 +1,20 @@
+### 一、基础环境
+
+linux Centos7 ，基础环境依赖自行安装: docker,docker-compose,python 2.7, git, wget
+
+### 二、搭建测试环境
+
+     wget https://raw.githubusercontent.com/actiontech/dble-test-suite/master/behave_dble/compose/start_env.sh
+     bash start_env.sh
+
+### 三、执行测试
+
+     wget https://raw.githubusercontent.com/actiontech/dble-test-suite/master/behave_dble/compose/start_dble_test.sh
+     bash start_dble_test.sh
+
+    注：该脚本覆盖了全部功能feature和全局表sql_cover的测试
+
+### 四、driver测试步骤请参考各driver下的readme.md
+
+
+
---
+++ behave_dble/compose/half-auto-env/install_mysql.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Copyright (C) 2016-2020 ActionTech.
+# License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
+#install mysql, the script is tested for mysql-5.7.13-linux-glibc2.5-x86_64.tar.gz
+echo "bash dir : $( dirname ${BASH_SOURCE[0]} )"
+
+rm -rf /usr/local/mysql
+tar -zxf mysql-*.tar.gz
+mysql_full_name=`ls | egrep -w "mysql*" | grep -v ".tar.gz"`
+echo "mysql install package to use: $mysql_full_name"
+mv ${mysql_full_name} /usr/local/mysql
+
+echo "groupadd"
+groupadd mysql
+useradd -r -g mysql -s /bin/false mysql
+cd /usr/local/mysql && mkdir data && chown -R mysql:mysql .
+#yum -y install libaio
+#yum -y install dos2unix
+
+echo "configure my.cnf!"
+cp /usr/local/mysql/support-files/my-default.cnf /etc/my.cnf
+sed -i '/mysqld_safe/,+2d' /etc/my.cnf
+sed -i -e '$a [client] \nuser=test \npassword=111111 \nhost=127.0.0.1 \n' -e "/\[mysqld\]/a server-id=$(($1+2)) \nsession_track_schema=1 \nsession_track_state_change=1 \nsession_track_system_variables=\"*\" \ngtid-mode=on \nenforce_gtid_consistency=on \nearly-plugin-load=keyring_file.so" /etc/my.cnf
+
+echo "mysql initialize!"
+rm -rf /var/lib/mysql
+/usr/local/mysql/bin/mysqld --initialize-insecure --basedir=/usr/local/mysql --user=mysql >/dev/null 2>&1
+
+echo "openssl install!"
+yum install -y openssl > /tmp/install_openssl.log
+yum install -y openssh-server
+
+echo "mysql_ssl_rsa_setup!"
+/usr/local/mysql/bin/mysql_ssl_rsa_setup > /tmp/mysql_ssl_rsa_setup.log
+
+echo "mysql start skip grant tables!"
+/usr/local/mysql/bin/mysqld_safe --defaults-file=/etc/my.cnf --skip-grant-tables --user=mysql >/dev/null 2>&1 &
+
+sleep 5s
+/usr/local/mysql/bin/mysql -uroot --skip-password -e "flush privileges;alter user root@'localhost' identified by '111111'"
+
+/usr/local/mysql/support-files/mysql.server restart
+
+echo "net-tools install!"
+yum install -y net-tools > /dev/null 2>&1
+
+rm -rf /usr/bin/mysql
+ln -s /usr/local/mysql/bin/mysql /usr/bin/mysql
