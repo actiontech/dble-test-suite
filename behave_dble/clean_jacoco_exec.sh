b8d17dfdafa41f058d36aabe90501f0a2b882a6b
wujinling <wujinling@actionsky.com>
2021-01-06 13:33:24 +0800

    change copyright to 2021

--- behave_dble/clean_jacoco_exec.sh
+++ behave_dble/clean_jacoco_exec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (C) 2016-2020 ActionTech.
+# Copyright (C) 2016-2021 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 #for code coverage, clean up the dble_jacoco.exec for every new ci instance runs
 
