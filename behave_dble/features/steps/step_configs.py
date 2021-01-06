738a9fbd6bb299e5c5ab021352f508ebbd976e36
wujinling <wujinling@actionsky.com>
2021-01-06 14:06:39 +0800

    change copyright to 2021

--- behave_dble/features/steps/step_configs.py
+++ behave_dble/features/steps/step_configs.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2016-2021 ActionTech.
+# License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 # @Time    : 2019/12/11 PM2:40
 # @Author  : zhaohongjie@actionsky.com
 # **********************
