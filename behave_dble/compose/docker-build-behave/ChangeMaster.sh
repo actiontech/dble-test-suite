78734120d1d4b1836b90a344ded0e36363b04db8
snowpiaojuan <hbxhjwj@yeah.net>
2021-01-07 11:28:07 +0800

    update ChangeMaster.sh

--- behave_dble/compose/docker-build-behave/ChangeMaster.sh
+++ behave_dble/compose/docker-build-behave/ChangeMaster.sh
@@ -9,9 +9,9 @@ echo "master ip is ${master_ip}"
 echo "==================  reset master ${1}  =================="
 ssh root@${1}  "/usr/local/mysql/bin/mysql -uroot -p111111 -h127.0.0.1 -P3306 -e \"reset master;stop slave;reset slave all;\" "
 ssh root@${1}  "sed -i -e '/log-bin=/d' -e '/binlog_format=/d' -e '/relay-log=/d' -e '/\[mysqld\]/a log-bin=mysql-bin \nbinlog_format=row \nrelay-log=mysql-relay-bin' /etc/my.cnf"
+ssh root@${1}  "/usr/local/mysql/support-files/mysql.server restart"
 ssh root@${1}  "/usr/local/mysql/bin/mysql -uroot -p111111 -e \"create user if not exists 'repl'@'%' identified by '111111';\""
 ssh root@${1}  "/usr/local/mysql/bin/mysql -uroot -p111111 -e \"grant replication slave on *.* to 'repl'@'%'\""
-ssh root@${1}  "/usr/local/mysql/support-files/mysql.server restart"
 
 sleep 60s
 
