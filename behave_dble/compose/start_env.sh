185b0c093af0a457eadf884d7e83383dedc8c9e4
wangjuan-action <75239673+wangjuan-action@users.noreply.github.com>
2021-01-11 17:41:49 +0800

    add for DBLE0REQ-102 about whiteIps (#257)
    Co-authored-by: snowpiaojuan <hbxhjwj@yeah.net>

--- behave_dble/compose/start_env.sh
+++ behave_dble/compose/start_env.sh
@@ -3,7 +3,8 @@
 mkdir /opt/behave/
 cd /opt/behave/ && git clone https://github.com/actiontech/dble-test-suite.git
 cd /opt/behave/dble-test-suite/behave_dble/compose/
-docker network create --gateway 172.100.9.253 --subnet 172.100.9.0/24 dble_test
+#docker network create --gateway 172.100.9.253 --subnet 172.100.9.0/24 dble_test
+docker network create -d bridge --ipv6 --subnet "2001:3984:3989::/64" --gateway "2001:3984:3989::1" --gateway 172.100.9.253 --subnet 172.100.9.0/24 dble_test
 docker-compose -f docker-compose.yml up -d --force
 docker exec driver-test sh -c "/etc/init.d/ssh start"
 docker exec -it behave bash "/init_assets/dble-test-suite/behave_dble/compose/docker-build-behave/init_test_env.sh"
