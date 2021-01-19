24ae1f18dd43bb363cb3879bc76a815303393033
wangjuan-action <75239673+wangjuan-action@users.noreply.github.com>
2021-01-19 17:54:54 +0800

    add for DBLE0REQ-814 (#270)
    * add for DBLE0REQ-814
    
    * remove capClientFoundRows=true
    
    Co-authored-by: snowpiaojuan <hbxhjwj@yeah.net>

--- drivers/java/sh/jdbc-api-test-start.sh
+++ drivers/java/sh/jdbc-api-test-start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Copyright (C) 2016-2021 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
-echo '=======           modify  capClientFoundRows=true and then restart dble                 ======='
-ssh root@dble-1 "sed -i '\$a -DcapClientFoundRows=true' /opt/dble/conf/bootstrap.cnf && /opt/dble/bin/dble restart"
+
 echo '=======          copy sys.config         ======='
 cp ../../java-interface/JDBCInterfaceTest/sys.config ../
 echo '=======            package               ======='
@@ -12,5 +11,3 @@ cp target/jdbc_api_test-1.0-jar-with-dependencies.jar ../java
 echo '=======     start jdbc_api_test          ======='
 cd ../java && java -jar jdbc_api_test-1.0-jar-with-dependencies.jar
 echo '=======    START SUCCESS jdbc_api_test   ======='
-echo '=======    restore capClientFoundRows to default   ======='
-ssh root@dble-1 "sed -i -e '/capClientFoundRows/d' /opt/dble/conf/bootstrap.cnf && /opt/dble/bin/dble restart"
