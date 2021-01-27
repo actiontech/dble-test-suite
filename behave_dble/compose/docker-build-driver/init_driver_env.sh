b1578b21911b20f7f8784e81765b04bb488527c6
wujinling <wujinling@actionsky.com>
2021-01-27 16:03:46 +0800

    fixed cover authorized_keys

--- behave_dble/compose/docker-build-driver/init_driver_env.sh
+++ behave_dble/compose/docker-build-driver/init_driver_env.sh
@@ -4,14 +4,14 @@ base_dir=$( dirname ${BASH_SOURCE[0]} )
 echo ${base_dir}
 
 #ssh with no pwd
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@dble-1:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@dble-2:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@dble-3:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql-master1:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql-master2:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@behave:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql8-master1:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql8-master2:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql8-slave1:/root/.ssh/authorized_keys
-sshpass -psshpass scp -o "StrictHostKeyChecking no" ~/.ssh/id_rsa.pub root@mysql8-slave2:/root/.ssh/authorized_keys
+sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@dble-1
+sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@dble-2
+sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@dble-3
+sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@mysql
+sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@mysql-master1
+sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@mysql-master2
+sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@mysql8-master1
+sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@mysql8-master2
+sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@mysql8-slave1
+sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@mysql8-slave2
+sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@behave
--- behave_dble/compose/start_env.sh
+++ behave_dble/compose/start_env.sh
@@ -6,8 +6,8 @@ cd /opt/behave/dble-test-suite/behave_dble/compose/
 #docker network create --gateway 172.100.9.253 --subnet 172.100.9.0/24 dble_test
 docker network create -d bridge --ipv6 --subnet "2001:3984:3989::/64" --gateway "2001:3984:3989::1" --gateway 172.100.9.253 --subnet 172.100.9.0/24 dble_test
 docker-compose -f docker-compose.yml up -d --force
+docker exec -it behave bash "/init_assets/dble-test-suite/behave_dble/compose/docker-build-behave/init_test_env.sh"
 docker exec driver-test sh -c "/etc/init.d/ssh start"
 docker cp docker-build-driver/init_driver_env.sh driver-test:/docker-build  && docker exec -it driver-test bash "/docker-build/init_driver_env.sh"
-docker exec -it behave bash "/init_assets/dble-test-suite/behave_dble/compose/docker-build-behave/init_test_env.sh"
 
 /bin/bash
