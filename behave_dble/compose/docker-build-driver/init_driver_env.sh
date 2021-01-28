8eb9f64e4beeab3cea4c9ce0dd31916e70affad4
wujinling <wujinling@actionsky.com>
2021-01-28 13:05:09 +0800

    fix start ssh service etc.

--- behave_dble/compose/docker-build-driver/init_driver_env.sh
+++ behave_dble/compose/docker-build-driver/init_driver_env.sh
@@ -4,14 +4,14 @@ base_dir=$( dirname ${BASH_SOURCE[0]} )
 echo ${base_dir}
 
 #ssh with no pwd
-sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@dble-1
-sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@dble-2
-sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@dble-3
-sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@mysql
-sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@mysql-master1
-sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@mysql-master2
-sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@mysql8-master1
-sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@mysql8-master2
-sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@mysql8-slave1
-sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@mysql8-slave2
-sshpass -psshpass ssh-copy-id -i ~/.ssh/id_rsa.pub root@behave
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@dble-1
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@dble-2
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@dble-3
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@mysql
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@mysql-master1
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@mysql-master2
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@mysql8-master1
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@mysql8-master2
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@mysql8-slave1
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@mysql8-slave2
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@behave
