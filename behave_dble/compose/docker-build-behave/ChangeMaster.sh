278d2cb6813d6ecdff0f5737689c2e7aab1fe965
quexiuping <quexiuping@actionsky.com>
2021-02-01 14:57:34 +0800

    fix zk_ha about DBLE0REQ-855

--- behave_dble/compose/docker-build-behave/ChangeMaster.sh
+++ behave_dble/compose/docker-build-behave/ChangeMaster.sh
@@ -11,9 +11,9 @@ ssh root@${1}  "/usr/local/mysql/bin/mysql -uroot -p111111 -h127.0.0.1 -P3306 -e
 ssh root@${1}  "sed -i -e '/log-bin=/d' -e '/binlog_format=/d' -e '/relay-log=/d' -e '/\[mysqld\]/a log-bin=mysql-bin \nbinlog_format=row \nrelay-log=mysql-relay-bin' /etc/my.cnf"
 ssh root@${1}  "/usr/local/mysql/support-files/mysql.server restart"
 ssh root@${1}  "/usr/local/mysql/bin/mysql -uroot -p111111 -e \"create user if not exists 'repl'@'%' identified by '111111';\""
-ssh root@${1}  "/usr/local/mysql/bin/mysql -uroot -p111111 -e \"grant replication slave on *.* to 'repl'@'%'\""
+ssh root@${1}  "/usr/local/mysql/bin/mysql -uroot -p111111 -e \"grant replication slave on *.* to 'repl'@'%';\""
 
-sleep 60s
+sleep 5s
 
 echo "==================  reset slave ${2}  =================="
 ssh root@${2}  "/usr/local/mysql/bin/mysql -uroot -p111111 -h127.0.0.1 -P3306 -e \"reset master;stop slave;reset slave all;set global gtid_purged='';\" "
