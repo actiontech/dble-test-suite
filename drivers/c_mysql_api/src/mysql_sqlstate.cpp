21a52fbb07d175355d22bc4b5d2b11fa84154e9a
quexiuping <quexiuping@actionsky.com>
2021-02-04 15:18:16 +0800

    fix c_api_test

--- drivers/c_mysql_api/src/mysql_sqlstate.cpp
+++ drivers/c_mysql_api/src/mysql_sqlstate.cpp
@@ -14,10 +14,10 @@ void case_mysql_sqlstate(MYSQL* conn)
 	if(mysql_query(conn, "use schema1/*a*/"))
 	{
 		const char * errState=mysql_sqlstate(conn);
-		printf("    pass! query 'use schema1/*a*/', mysql_sqlstate: %s\n", errState);
+		printf("    pass! query 'use schema1/*a*/', success\n");
 	}else{
         if(!IS_DEBUG){
-            printf("expect send select to master get err, but success\n");
+            printf("expect send select to master success, but get err\n");
         	exit(1);
         }
 	}
@@ -25,17 +25,18 @@ void case_mysql_sqlstate(MYSQL* conn)
 	MYSQL* connErr = mysql_init(NULL);
     mysql_real_connect(connErr, HOST_DBLE, ADMIN, ADMIN_PASSWD, "", DBLE_ADMIN_PORT, NULL, CLIENT_DEPRECATE_EOF);
 
-    if (mysql_query(connErr, "show databases")) {
+    if (mysql_query(connErr, "show databases")!=0)
+    {
 		const char * errState=mysql_sqlstate(connErr);
         const char* err=mysql_error(connErr);
         if(strcmp("Unsupported statement", err)!=0){
-            printf("expect 'Unsupported statement.', but %s\n", err);
+            printf("expect 'success', but %s\n", err);
             exit(1);
         }else{
-            printf("    pass! send query to admin port, mysql_sqlstate: %s\n", errState);
+            printf("expect 'success',  mysql_sqlstate: %s\n", errState);
+            exit(1);
         }
 	}else{
-        printf("expect Unsupported statement, but success\n");
-		exit(1);
+	    printf("    pass! send query 'show databases' to admin port, success\n");
 	}
 }
