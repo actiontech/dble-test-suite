3e4f5f9f89887713f8dec4c1173df209668aec02
snowpiaojuan <hbxhjwj@yeah.net>
2021-02-22 13:28:15 +0800

    update fix_mysql_stop

--- behave_dble/features/steps/lib/MySQLObject.py
+++ behave_dble/features/steps/lib/MySQLObject.py
@@ -54,13 +54,14 @@ class MySQLObject(object):
             stop_cd, stop_out, stop_err = self._mysql_meta.ssh_conn.exec_command(cmd_stop)
             success_p = "Shutting down MySQL.*?SUCCESS"
             obj = re.search(success_p, stop_out)
-            isSuccess = obj is not None
-            if isSuccess != 1:
+
+            if obj is None:
+                logger.debug("stop mysql err:{}".format(stop_err))
                 cmd_kill = "kill -9 `ps -ef | grep mysqld |grep -v grep | awk '{print $2}'`"
                 rc, sto, ste = self._mysql_meta.ssh_conn.exec_command(cmd_kill)
                 assert len(ste) == 0, "kill mysql failed for: {0}".format(ste)
             else:
-                assert isSuccess, "stop mysql err:{}".format(stop_err)
+                logger.debug("stop mysql success => {0}".format(self._mysql_meta.ip))
 
         self._mysql_meta.close_ssh()
 
