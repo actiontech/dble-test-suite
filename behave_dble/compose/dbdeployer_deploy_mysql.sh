b49a3e17eac1c456959af35a9323cc80cbe5dc44
wjl1619 <401172143@qq.com>
2021-03-23 15:48:00 +0800

    amend for initial envir

--- behave_dble/compose/dbdeployer_deploy_mysql.sh
+++ behave_dble/compose/dbdeployer_deploy_mysql.sh
@@ -10,8 +10,10 @@ echo "mysql_version is: ${mysql_version}"
 
 # initial mysql
 #dbdeployer delete --skip-confirm ALL
-dbdeployer deploy single ${1} --remote-access % --bind-address 0.0.0.0 -c skip-name-resolve --port-as-server-id --port 3307 -p 111111 -u test --force
+dbdeployer deploy single ${1} --remote-access % --bind-address 0.0.0.0 -c skip-name-resolve --port-as-server-id --port 3307 -p 111111 -u test --gtid --force
 
 #create soft link
 rm -rf /usr/bin/mysql && ln -sf /root/opt/mysql/${1}/bin/mysql /usr/bin/mysql
 rm -f /tmp/mysql.sock  && ln -s /tmp/mysql_sandbox3307.sock /tmp/mysql.sock
+
+echo "mysql all install success"
