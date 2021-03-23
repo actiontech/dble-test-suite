a31f109d79621980d43ad3f3b7d87f785131a5f1
wjl1619 <401172143@qq.com>
2021-03-23 15:48:00 +0800

    amend for initial envir

--- behave_dble/compose/docker-build-behave/ChangeMaster.sh
+++ behave_dble/compose/docker-build-behave/ChangeMaster.sh
@@ -8,20 +8,20 @@ echo "master ip is ${master_ip}"
 mysql_version=`echo ${4} | sed 's/\./\_/g'`
 
 echo "==================  reset master ${1}  =================="
-ssh root@${1}  "mysql -utest -p111111 -h127.0.0.1 -P3306 -e \"reset master;stop slave;reset slave all;\" "
+ssh root@${1}  "mysql -uroot -p111111 -e \"reset master;stop slave;reset slave all;\" "
 ssh root@${1}  "sed -i -e '/log-bin=/d' -e '/binlog_format=/d' -e '/relay-log=/d' -e '/\[mysqld\]/a log-bin=mysql-bin \nbinlog_format=row \nrelay-log=mysql-relay-bin' /root/sandboxes/msb_${mysql_version}/my.sandbox.cnf"
 ssh root@${1}  "/root/sandboxes/msb_${mysql_version}/restart"
-ssh root@${1}  "mysql -utest -p111111 -e \"create user if not exists 'repl'@'%' identified by '111111';\""
-ssh root@${1}  "mysql -utest -p111111 -e \"grant replication slave on *.* to 'repl'@'%';\""
+ssh root@${1}  "mysql -uroot -p111111 -e \"create user if not exists 'repl'@'%' identified by '111111';\""
+ssh root@${1}  "mysql -uroot -p111111 -e \"grant replication slave on *.* to 'repl'@'%';\""
 
 sleep 5s
 
 echo "==================  reset slave ${2}  =================="
-ssh root@${2}  "mysql -utest -p111111 -h127.0.0.1 -P3306 -e \"reset master;stop slave;reset slave all;set global gtid_purged='';\" "
-ssh root@${2}  "mysql -utest -p111111 -e \"change master to master_host='${master_ip}', master_user='repl', master_password='111111', master_auto_position=1;\""
-ssh root@${2}  "mysql -utest -p111111 -h127.0.0.1 -P3306 -e \"start slave;\" "
+ssh root@${2}  "mysql -uroot -p111111 -e \"reset master;stop slave;reset slave all;set global gtid_purged='';\" "
+ssh root@${2}  "mysql -uroot -p111111 -e \"change master to master_host='${master_ip}', master_user='repl', master_password='111111', master_auto_position=1;\""
+ssh root@${2}  "mysql -uroot -p111111 -e \"start slave;\" "
 
 echo "==================  reset slave ${3}  =================="
-ssh root@${3}  "mysql -utest -p111111 -h127.0.0.1 -P3306 -e \"reset master;stop slave;reset slave all;set global gtid_purged='';\" "
-ssh root@${3}  "mysql -utest -p111111 -e \"change master to master_host='${master_ip}', master_user='repl', master_password='111111', master_auto_position=1;\""
-ssh root@${3}  "mysql -utest -p111111 -h127.0.0.1 -P3306 -e \"start slave;\" "
+ssh root@${3}  "mysql -uroot -p111111 -e \"reset master;stop slave;reset slave all;set global gtid_purged='';\" "
+ssh root@${3}  "mysql -uroot -p111111 -e \"change master to master_host='${master_ip}', master_user='repl', master_password='111111', master_auto_position=1;\""
+ssh root@${3}  "mysql -uroot -p111111 -e \"start slave;\" "
--- behave_dble/compose/docker-build-behave/init_test_env.sh
+++ behave_dble/compose/docker-build-behave/init_test_env.sh
@@ -18,7 +18,7 @@ sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub
 #init mysql passwd
 mysql_install=("mysql" "mysql-master1" "mysql-master2" "dble-1" "dble-2" "dble-3" "mysql8-master1" "mysql8-master2" "mysql8-slave1" "mysql8-slave2")
 count=${#mysql_install[@]}
-for((i=0; i<5; i=i+1)); do
+for((i=0; i<=5; i=i+1)); do
     ssh root@${mysql_install[$i]}  "bash /docker-build/dbdeployer_deploy_mysql.sh 5.7.25"
 done
 for((i=6; i<count; i=i+1)); do
