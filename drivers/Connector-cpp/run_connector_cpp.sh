ba5fd6ad1173a97c703382428ec0d167b3d3e3a1
wjl1619 <401172143@qq.com>
2021-04-13 15:54:34 +0800

    modified for drivers

--- drivers/Connector-cpp/run_connector_cpp.sh
+++ drivers/Connector-cpp/run_connector_cpp.sh
@@ -8,7 +8,7 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd ${DIR}/../../behave_dble/compose/docker-build-behave && bash resetReplication.sh
 
 #restart dble
-ssh root@behave "cd /var/lib/go-agent/pipelines/autotest-dble-${dble_version}/behave_dble;behave --stop -D dble_conf=sql_cover_sharding features/setup.feature;chown -R go:go dble_conf/sql_cover_sharding;chown -R go:go logs"
+ssh root@behave "cd /var/lib/go-agent/pipelines/autotest-dble-${dble_version}/behave_dble;pipenv run behave --stop -D dble_conf=sql_cover_sharding features/setup.feature;chown -R go:go dble_conf/sql_cover_sharding;chown -R go:go logs"
 
 #compile code
 cd ${DIR}/src && g++ *.cpp -l mysqlcppconn -l yaml-cpp
