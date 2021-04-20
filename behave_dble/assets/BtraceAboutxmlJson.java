847eab7057855a2628a3317c4519138d9b2f4f09
quexiuping <quexiuping@actionsky.com>
2021-04-20 10:21:11 +0800

    add case

---
+++ behave_dble/assets/BtraceAboutxmlJson.java
@@ -0,0 +1,23 @@
+package com.actiontech.dble.btrace.script;
+
+import com.sun.btrace.BTraceUtils;
+import com.sun.btrace.annotations.*;
+
+
+@BTrace(unsafe = true)
+public final class BtraceAboutxmlJson {
+    private BtraceAboutxmlJson() {
+    }
+
+    @OnMethod(
+            clazz = "com.actiontech.dble.config.ServerConfig",
+            method = "syncJsonToLocal"
+    )
+    public static void syncJsonToLocal(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
+        BTraceUtils.println("get into sleep");
+        BTraceUtils.println("---------------");
+        Thread.sleep(20000L);
+        BTraceUtils.println("sleep end ");
+        BTraceUtils.println("---------------");
+    }
+}
