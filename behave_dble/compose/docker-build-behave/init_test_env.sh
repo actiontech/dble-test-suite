17c7549c9e03b9447429b162830a8faf8df8b71a
wjl1619 <401172143@qq.com>
2021-05-07 10:29:04 +0800

    fix create replication fail

--- behave_dble/compose/docker-build-behave/init_test_env.sh
+++ behave_dble/compose/docker-build-behave/init_test_env.sh
@@ -16,7 +16,7 @@ sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub
 sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@mysql8-slave2
 
 #init mysql passwd
-mysql_install=("mysql" "mysql-master1" "mysql-master2" "dble-1" "dble-2" "dble-3" "mysql8-master1" "mysql8-master2" "mysql8-slave1" "mysql8-slave2")
+mysql_install=("dble-1" "dble-2" "dble-3" "mysql" "mysql-master1" "mysql-master2" "mysql8-master1" "mysql8-master2" "mysql8-slave1" "mysql8-slave2")
 count=${#mysql_install[@]}
 for((i=0; i<=5; i=i+1)); do
   server_id=`expr ${i} + 1`
