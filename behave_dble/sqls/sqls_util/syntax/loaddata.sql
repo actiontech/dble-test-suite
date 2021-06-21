da05ae81eef274688f91d8bb19a1aced1443a878
snowpiaojuan <hbxhjwj@yeah.net>
2021-06-21 15:56:15 +0800

    fix ci issue for sql support

--- behave_dble/sqls/sqls_util/syntax/loaddata.sql
+++ behave_dble/sqls/sqls_util/syntax/loaddata.sql
@@ -24,8 +24,8 @@ load data infile "./test3.txt" into table test1 character set 'utf8' fields term
 select id,c1,c2,c3,c4,c5 from test1 order by id
 ##github #774
 drop table if exists test1
-create table test1(id int, c1 char(5), c2 char(5), c3 char(5));
-load data infile "./test4.txt" replace into table test1 fields terminated by ',' lines terminated by '\n' (id,c1) set c2='c2', c3='c3';
+create table test1(id int, c1 char(5), c2 char(5), c3 char(5)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+load data infile "./test4.txt" replace into table test1 character set 'utf8' fields terminated by ',' lines terminated by '\n' (id,c1) set c2='c2', c3='c3';
 select id,c1,c2,c3 from test1;
 ##more than 10000+ line in test.txt
 load data infile "./test.txt" into table test1 character set 'utf8' fields terminated by ',' lines terminated by '\n'
