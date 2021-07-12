b029d8045f8c17060f3723071c0cf577ad4c6cd1
snowpiaojuan <hbxhjwj@yeah.net>
2021-07-12 18:31:04 +0800

    fix ci issue - driver_java

--- drivers/Connector-.Net/run_connector_dotnet.sh
+++ drivers/Connector-.Net/run_connector_dotnet.sh
@@ -2,18 +2,19 @@
 # Copyright (C) 2016-2021 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 
+echo '=======                 restore the running environment              ======='
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd ${DIR}/../../behave_dble/compose/docker-build-behave && bash resetReplication.sh
 
-#restart dble
+echo '=======           restart dble with sql_cover_sharding               ======='
 cd ${DIR}/../../behave_dble && behave --stop -D dble_conf=sql_cover_sharding features/setup.feature
 
-#compile code
+echo '=======                       compile                                ======='
 cd ${DIR}/netdriver && csc -out:test.exe -r:MySql.Data.dll -r:YamlDotNet.dll *.cs
 
-#do run cpp driver test
+echo '=======                        driver test                           ======='
 cd ${DIR} && bash do_run_connector_dotnet.sh -c
 
-#save logs for ci artifacts
+echo '=======                   save logs for ci artifacts                 ======='
 scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
 mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
--- drivers/Connector-J/run_connector_J.sh
+++ drivers/Connector-J/run_connector_J.sh
@@ -2,10 +2,11 @@
 # Copyright (C) 2016-2021 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 
+echo '=======                 restore the running environment              ======='
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd ${DIR}/../../behave_dble/compose/docker-build-behave && bash resetReplication.sh
 
-#restart dble
+echo '=======           restart dble with sql_cover_sharding               ======='
 cd ${DIR}/../../behave_dble && behave --stop -D dble_conf=sql_cover_sharding features/setup.feature
 
 echo '=======                       package                                ======='
