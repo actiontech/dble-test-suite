adccdeda8cb389bdba0a17567ff55249027c5efd
caiwei <caiwei@actionsky.com>
2021-07-30 15:23:58 +0800

    add case about connection leak test
    (cherry picked from commit 9b9b63e5f43bdf01f3049583bfdd13135164966b)

---
+++ behave_dble/assets/BtraceLineDelay.java
@@ -0,0 +1,18 @@
+package com.actiontech.dble.btrace.script;
+
+import com.sun.btrace.BTraceUtils;
+import com.sun.btrace.annotations.*;
+
+
+@BTrace(unsafe = true)
+public class BtraceLineDelay{
+    @OnMethod(
+            clazz="com.actiontech.dble.sqlengine.MultiTablesMetaJob",
+            location=@Location(value=Kind.LINE,line=157)
+    )
+    public static void args(@Self Object self, @ProbeMethodName String pmn, int line) throws Exception {
+        BTraceUtils.print("delay for NP test");
+        BTraceUtils.print("for print order ...................");
+        Thread.sleep(10000L);
+    }
+}
