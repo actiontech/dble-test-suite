1899493d1d806654b9fae750907c589c9a85b014
snowpiaojuan <hbxhjwj@yeah.net>
2021-08-24 14:01:48 +0800

    update ci task bash

--- behave_dble/run_func.sh
+++ behave_dble/run_func.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Copyright (C) 2016-2021 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
-behave -Dreset=true -D dble_conf=template features/install_uninstall/install_dble.feature features/func_test/global_var_init/ features/func_test/load_data/  features/func_test/safety/  features/func_test/sequence/  features/func_test/sharding_func_test/  features/func_test/slow_log/  features/func_test/special/ features/func_test/sql_plan/ features/func_test/trace/  features/func_test/heartbeat/ features/func_test/metalock/
+behave -Dreset=true -D dble_conf=template features/install_uninstall/install_dble.feature features/func_test
--- behave_dble/run_func_unstable.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Copyright (C) 2016-2021 ActionTech.
-# License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
-behave -Dreset=true -D dble_conf=template features/install_uninstall/install_dble.feature features/func_test/cfg_test/ features/func_test/dble_information/ features/func_test/managercmd/ features/func_test/flow_control/ features/func_test/connection/  features/func_test/ddl/  features/func_test/xa_transaction/ features/func_test/Large_Packet/
