04b4ad4cd16c4b8f82b76a77004654951006baaf
snowpiaojuan <hbxhjwj@yeah.net>
2021-08-26 16:33:14 +0800

    add ci task bash

---
+++ behave_dble/run_cfg_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Copyright (C) 2016-2021 ActionTech.
+# License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
+behave -Dreset=true -D dble_conf=template features/install_uninstall/install_dble.feature features/func_test/cfg_test/
