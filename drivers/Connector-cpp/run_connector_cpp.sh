ea72c275e3775c0ad3d8098012a7f3e4df69fd8c
wjl1619 <44627441+wjl1619@users.noreply.github.com>
2021-09-01 16:21:25 +0800

    fix Host key verification failed (#358)

--- drivers/Connector-cpp/run_connector_cpp.sh
+++ drivers/Connector-cpp/run_connector_cpp.sh
@@ -3,13 +3,11 @@ set -e
 # Copyright (C) 2016-2021 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 
-dble_version=$1
-
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd ${DIR}/../../behave_dble/compose/docker-build-behave && bash resetReplication.sh
 
 #restart dble
-ssh root@behave "cd /var/lib/go-agent/pipelines/autotest-dble-${dble_version}/behave_dble;pipenv run behave --stop -D dble_conf=sql_cover_sharding features/setup.feature;chown -R go:go dble_conf/sql_cover_sharding;chown -R go:go logs"
+cd ${DIR}/../../behave_dble&&pipenv run behave --stop -D dble_conf=sql_cover_sharding features/setup.feature
 
 #compile code
 cd ${DIR}/src && g++ *.cpp -l mysqlcppconn -l yaml-cpp
--- drivers/c_mysql_api/multi-queries/multiQuery.sh
+++ drivers/c_mysql_api/multi-queries/multiQuery.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
-dble_version=$1
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd ${DIR}/../../../behave_dble/compose/docker-build-behave && bash resetReplication.sh
 
 #restart dble
-ssh root@behave "cd /var/lib/go-agent/pipelines/autotest-dble-${dble_version}/behave_dble;pipenv run behave --stop -D dble_conf=sql_cover_sharding features/setup.feature;chown -R go:go dble_conf/sql_cover_sharding;chown -R go:go logs"
+cd ${DIR}/../../../behave_dble&&pipenv run behave --stop -D dble_conf=sql_cover_sharding features/setup.feature
 
 #compile multiquery code
 cd ${DIR} && make clean && make
