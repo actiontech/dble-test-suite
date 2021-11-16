688517d1432ea8940ca67a10c2a8893a3e9aef5e
caiwei <70261656+jndxcaiwei@users.noreply.github.com>
2021-11-16 18:07:39 +0800

    solve problem about connection not close used in thread (#407)

--- behave_dble/sqls/manager/manager.sql
+++ behave_dble/sqls/manager/manager.sql
@@ -2,6 +2,7 @@
 select @@VERSION_COMMENT
 select @@SESSION.TX_READ_ONLY
 select @@max_allowed_packet
+select TIMEDIFF(NOW(), UTC_TIMESTAMP())
 
 #set
 set @a=20；
@@ -59,3 +60,11 @@ show @@time.startup
 show @@version
 show @@white
 SHOW VARIABLES WHERE Variable_name ='language' OR Variable_name = 'net_write_timeout' OR Variable_name = 'interactive_timeout' OR Variable_name = 'wait_timeout' OR Variable_name = 'character_set_client' OR Variable_name = 'character_set_connection' OR Variable_name = 'character_set' OR Variable_name = 'character_set_server' OR Variable_name = 'tx_isolation' OR Variable_name = 'transaction_isolation' OR Variable_name = 'character_set_results' OR Variable_name = 'timezone' OR Variable_name = 'time_zone' OR Variable_name = 'system_time_zone' OR Variable_name = 'lower_case_table_names' OR Variable_name = 'max_allowed_packet' OR Variable_name = 'net_buffer_length' OR Variable_name = 'sql_mode' OR Variable_name = 'query_cache_type' OR Variable_name = 'query_cache_size' OR Variable_name = 'init_connect';
+show @@pause
+show COLLATION
+show @@alert
+show @@reload_status
+show @@user.privilege
+show @@user
+show @@session.xa
+show @@ddl
