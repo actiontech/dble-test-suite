4a742def49f30b1bb5ebcb1abded80842793c0b8
snowpiaojuan <hbxhjwj@yeah.net>
2021-12-01 10:49:59 +0800

    fix ci issue

---
+++ drivers/java/sh/jdbc-api-test-start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright (C) 2016-2021 ActionTech.
+# License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
+
+echo '=======          copy sys.config         ======='
+cp ../../java-interface/JDBCInterfaceTest/sys.config ../
+echo '=======            package               ======='
+cd ../../jdbc-api-test && /usr/local/apache-maven-3.6.3/bin/mvn -DskipTest clean package assembly:assembly
+echo '=======    copy jdbc_api_test-1.0-jar-with-dependencies.jar    ======='
+cp target/jdbc_api_test-1.0-jar-with-dependencies.jar ../java
+echo '=======     start jdbc_api_test          ======='
+cd ../java && java -jar jdbc_api_test-1.0-jar-with-dependencies.jar
+echo '=======    START SUCCESS jdbc_api_test   ======='
