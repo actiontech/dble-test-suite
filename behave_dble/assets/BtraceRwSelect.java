852d7db9103afadc82e294c18a0303431050cca6
caiwei <jndxcw@163.com>
2022-01-12 10:24:31 +0800

    add case about rwSplit connection test

---
+++ behave_dble/assets/BtraceRwSelect.java
@@ -0,0 +1,21 @@
+package btrace;
+
+import com.sun.btrace.BTraceUtils;
+import com.sun.btrace.annotations.BTrace;
+import com.sun.btrace.annotations.OnMethod;
+import com.sun.btrace.annotations.ProbeClassName;
+import com.sun.btrace.annotations.ProbeMethodName;
+
+@BTrace(unsafe = true)
+public class BtraceRwSelect{
+
+    @OnMethod(
+            clazz = "com.actiontech.dble.backend.datasource.PhysicalDbGroup",
+            method = "rwSelect"
+    )
+    public static void handle(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod) throws Exception {
+       BTraceUtils.println("get into rwSelect");
+       BTraceUtils.println("------------------------");
+       Thread.sleep(2000L);
+    }
+}
