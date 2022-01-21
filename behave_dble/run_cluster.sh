f86dc9ac39e9d778b2c67d4c68732a7863d5f7f5
chenhuiming <775372845@qq.com>
2022-01-21 17:50:40 +0800

    update copyright to 2022

--- behave_dble/run_cluster.sh
+++ behave_dble/run_cluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Copyright (C) 2016-2021 ActionTech.
+# Copyright (C) 2016-2022 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 behave -Dis_cluster=true -Dreset=true features/install_uninstall/single_dble_and_zk_cluster.feature features/install_uninstall/install_dble_cluster.feature features/cluster/ features/install_uninstall/stop_dble_cluster.feature
--- behave_dble/run_func.sh
+++ behave_dble/run_func.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Copyright (C) 2016-2021 ActionTech.
+# Copyright (C) 2016-2022 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 behave -Dreset=true -D dble_conf=template features/install_uninstall/install_dble.feature features/func_test
