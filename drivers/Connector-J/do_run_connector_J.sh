0f316a6e9d182f3ce41780b89c042ae82fbc7857
wangjuan <75239673+wangjuan-action@users.noreply.github.com>
2022-02-16 18:13:17 +0800

    fix ci issue - update script and sql (#474)

--- drivers/Connector-J/do_run_connector_J.sh
+++ drivers/Connector-J/do_run_connector_J.sh
@@ -8,6 +8,7 @@ need_compare=${2-"false"}
 
 echo $jar_name""
 
+export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Duser.language=en   -Dfile.encoding=UTF8 "
 java -jar target/${jar_name} "" "conf/auto_dble_test.yaml" "driver_test_client.sql" "driver_test_manager.sql"
 
 if [ "$need_compare" = "-c" ]; then
