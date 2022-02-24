1ed4167de476aee3d84631559e549924db71895f
chenhuiming <chenhuiming@actionsky.com>
2022-02-24 10:54:26 +0800

    update copyright to 2022

--- drivers/Connector-python/src/preprocess.py
+++ drivers/Connector-python/src/preprocess.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright (C) 2016-2021 ActionTech.
+# Copyright (C) 2016-2022 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 import os
 import time
