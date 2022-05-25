b8ffbfcd0bcbe19472023afd852f2f69949d2553
caiwei <jndxcw@163.com>
2022-05-25 11:47:47 +0800

    add exit flag for ci check status success

--- drivers/Connector-.Net/run_connector_dotnet.sh
+++ drivers/Connector-.Net/run_connector_dotnet.sh
@@ -13,8 +13,19 @@ echo '=======                       compile                                =====
 cd ${DIR}/netdriver && csc -out:test.exe -r:MySql.Data.dll -r:YamlDotNet.dll *.cs
 
 echo '=======                        driver test                           ======='
-cd ${DIR} && bash do_run_connector_dotnet.sh -c
+cd ${DIR} && source do_run_connector_dotnet.sh -c
+if [ ${#res} -gt 0 ]; then
+    echo "Oop! results are different with the standard ones, try 'diff -qwr ${cmp_std_res_dir}/.../file1 ${cmp_real_res_dir}/.../file2' to see the details"
+    echo "${res}"
+     #save logs for ci artifacts
+    scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
+    mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
+    exit 1
+else
+    echo "pass"
+     #save logs for ci artifacts
+    scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
+    mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
+    exit 0
+fi
 
-echo '=======                   save logs for ci artifacts                 ======='
-scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
-mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
