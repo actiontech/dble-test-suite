ad6b9509c84b2e528ef9e76c71d2c301bdc44da9
caiwei <jndxcw@163.com>
2022-05-25 11:47:47 +0800

    add exit flag for ci check status success

--- drivers/Connector-.Net/run_connector_dotnet.sh
+++ drivers/Connector-.Net/run_connector_dotnet.sh
@@ -2,18 +2,30 @@
 # Copyright (C) 2016-2022 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 
+echo '=======                 restore the running environment              ======='
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd ${DIR}/../../behave_dble/compose/docker-build-behave && bash resetReplication.sh
 
-#restart dble
+echo '=======           restart dble with sql_cover_sharding               ======='
 cd ${DIR}/../../behave_dble && behave --stop -D dble_conf=sql_cover_sharding features/setup.feature
 
-#compile code
+echo '=======                       compile                                ======='
 cd ${DIR}/netdriver && csc -out:test.exe -r:MySql.Data.dll -r:YamlDotNet.dll *.cs
 
-#do run cpp driver test
-cd ${DIR} && bash do_run_connector_dotnet.sh -c
+echo '=======                        driver test                           ======='
+cd ${DIR} && source do_run_connector_dotnet.sh -c
+if [ ${#res} -gt 0 ]; then
+    echo "Oop! results are different with the standard ones, try 'diff -qwr ${cmp_std_res_dir}/.../file1 ${cmp_real_res_dir}/.../file2' to see the details"
+    echo "${res}"
+     #save logs for ci artifacts
+    scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
+    mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
+    exit 1
+else
+    echo "pass"
+     #save logs for ci artifacts
+    scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
+    mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
+    exit 0
+fi
 
-#save logs for ci artifacts
-scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
-mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
--- drivers/Connector-J/run_connector_J.sh
+++ drivers/Connector-J/run_connector_J.sh
@@ -2,18 +2,29 @@
 # Copyright (C) 2016-2022 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 
+echo '=======                 restore the running environment              ======='
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd ${DIR}/../../behave_dble/compose/docker-build-behave && bash resetReplication.sh
 
-#restart dble
+echo '=======           restart dble with sql_cover_sharding               ======='
 cd ${DIR}/../../behave_dble && behave --stop -D dble_conf=sql_cover_sharding features/setup.feature
 
-#driver java test code is compiled in connector_j.jar
-cp /var/lib/go-agent/pipelines/connector_j.jar ${DIR}/target/
+echo '=======                       package                                ======='
+cd ${DIR} && /usr/local/apache-maven-3.6.3/bin/mvn -DskipTest clean package assembly:assembly
 
-#do run driver test
-cd ${DIR} && bash do_run_connector_J.sh connector_j.jar -c
-
-#save logs for ci artifacts
-scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
-mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
+echo '=======                        driver test                           ======='
+cd ${DIR}/ && source do_run_connector_J.sh Jconnector-5.1.35-jar-with-dependencies.jar -c
+if [ ${#res} -gt 0 ]; then
+    echo "Oop! results are different with the standard ones, try 'diff -qwr ${cmp_std_res_dir}/.../file1 ${cmp_real_res_dir}/.../file2' to see the details"
+    echo "${res}"
+    #save logs for ci artifacts
+    scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
+    mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
+    exit 1
+else
+    echo "pass"
+    #save logs for ci artifacts
+    scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
+    mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
+    exit 0
+fi
