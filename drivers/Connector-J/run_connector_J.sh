7050dd7e4a73c926f31a1ee3b3371b188847e8eb
caiwei <jndxcw@163.com>
2022-05-25 11:47:47 +0800

    add exit flag for ci check status success

--- drivers/Connector-J/run_connector_J.sh
+++ drivers/Connector-J/run_connector_J.sh
@@ -13,8 +13,18 @@ echo '=======                       package                                =====
 cd ${DIR} && /usr/local/apache-maven-3.6.3/bin/mvn -DskipTest clean package assembly:assembly
 
 echo '=======                        driver test                           ======='
-cd ${DIR}/ && bash do_run_connector_J.sh Jconnector-5.1.35-jar-with-dependencies.jar -c
-
-echo '=======                   save logs for ci artifacts                 ======='
-scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
-mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
+cd ${DIR}/ && source do_run_connector_J.sh Jconnector-5.1.35-jar-with-dependencies.jar -c
+if [ ${#res} -gt 0 ]; then
+    echo "Oop! results are different with the standard ones, try 'diff -qwr ${cmp_std_res_dir}/.../file1 ${cmp_real_res_dir}/.../file2' to see the details"
+    echo "${res}"
+    #save logs for ci artifacts
+    scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
+    mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
+    exit 1
+else
+    echo "pass"
+    #save logs for ci artifacts
+    scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
+    mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
+    exit 0
+fi
