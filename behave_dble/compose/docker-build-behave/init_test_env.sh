3ec33a5475a536730d06d1ab5cf942b6beb50e26
asiro <asiro@qq.com>
2022-06-02 14:56:24 +0800

    init docker in docker

--- behave_dble/compose/docker-build-behave/init_test_env.sh
+++ behave_dble/compose/docker-build-behave/init_test_env.sh
@@ -4,19 +4,19 @@ base_dir=$( dirname ${BASH_SOURCE[0]} )
 echo ${base_dir}
 
 #ssh with no pwd
-sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@dble-1
-sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@dble-2
-sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@dble-3
-sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@mysql
-sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@mysql-master1
-sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@mysql-master2
-sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@mysql8-master1
-sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@mysql8-master2
-sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@mysql8-slave1
-sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@mysql8-slave2
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@172.100.9.1
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@172.100.9.2
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@172.100.9.3
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@172.100.9.4
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@172.100.9.5
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@172.100.9.6
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@172.100.9.9
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@172.100.9.10
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@172.100.9.11
+sshpass -psshpass ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa.pub root@172.100.9.12
 
 #init mysql passwd
-mysql_install=("dble-1" "dble-2" "dble-3" "mysql" "mysql-master1" "mysql-master2" "mysql8-master1" "mysql8-master2" "mysql8-slave1" "mysql8-slave2")
+mysql_install=("172.100.9.1" "172.100.9.2" "172.100.9.3" "172.100.9.4" "172.100.9.5" "172.100.9.6" "172.100.9.9" "172.100.9.10" "172.100.9.11" "172.100.9.12")
 count=${#mysql_install[@]}
 for((i=0; i<=5; i=i+1)); do
   server_id=`expr ${i} + 1`
@@ -30,7 +30,7 @@ done
 bash ${base_dir}/resetReplication.sh
 
 #config zookeeper's myid
-dble_install=("dble-1" "dble-2" "dble-3")
+dble_install=("172.100.9.1" "172.100.9.2" "172.100.9.3")
 count=${#dble_install[@]}
 for((i=0; i<3; i=i+1)); do
   myid=`expr ${i} + 1`
