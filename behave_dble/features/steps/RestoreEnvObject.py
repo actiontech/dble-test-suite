cd8efff716e5388ce07b0783a3754f68507fbece
caiwei <jndxcw@163.com>
2022-07-01 17:46:24 +0800

    optimize delete mysql tables (#540)
    (cherry picked from commit e813daa402495da418b7b6033fadd8222424f5a9)

--- behave_dble/features/steps/RestoreEnvObject.py
+++ behave_dble/features/steps/RestoreEnvObject.py
@@ -5,16 +5,24 @@
 # @Author  : irene-coming
 import logging
 
-from lib import utils
+from steps.lib import utils
 
 from steps.lib.ObjectFactory import ObjectFactory
 from steps.mysql_steps import execute_sql_in_host
+# from steps.lib.utils import get_ssh
+from steps.lib.utils import get_sftp, get_ssh, get_node
+
+import re
+
+from hamcrest import *
 
 logger = logging.getLogger('root')
+
+
 class RestoreEnvObject(object):
-    def __init__(self,scenario):
+    def __init__(self, scenario):
         self._scenario = scenario
-        
+
     def restore(self):
         # if "restore_sys_time" in self._scenario.tags:
         #     utils.restore_sys_time()
@@ -22,6 +30,45 @@ class RestoreEnvObject(object):
         # if "aft_reset_replication" in self._scenario.tags:
         #     utils.reset_repl()
         #
+        if "restore_network" in self._scenario.tags:
+            params_dic = self.get_tag_params("{'restore_network'")
+            logger.debug("params_dic is: {0}".format(params_dic))
+            if params_dic:
+                paras = params_dic["restore_network"].split(",")
+                # paras = paras.split(",")
+            else:
+                paras = ""
+
+            logger.debug("try to restore_network: {0}".format(paras))
+            for host_name in paras:
+                logger.debug("the value of host_name is: {0}".format(host_name))
+                cmd = "iptables -F"
+                ssh = get_ssh(host_name)
+                rc, stdout, stderr = ssh.exec_command(cmd)
+                assert_that(len(stderr) == 0, "restore network with command:{1}, got err:{0}".format(stderr, cmd))
+
+        if "restore_view" in self._scenario.tags:
+            params_dic = self.get_tag_params("{'restore_view'")
+            if params_dic:
+                paras = params_dic["restore_view"]
+            else:
+                paras = {}
+
+            for host_name, mysql_vars in paras.items():
+                if host_name.find('dble') != -1:
+                    mode = "user"
+                else:
+                    mode = "mysql"
+                for k, v in mysql_vars.items():
+                    list_value = filter(lambda x: x, v.split(","))
+                    view_value = ""
+                    for value in list_value:
+                        view_value = view_value + "{0}.{1},".format(k, value)
+                    query = "drop view if exists " + view_value
+
+                sql = query[:-1]  # delete the last ','
+                # logger.debug("the sql is: {0}".format(sql))
+                execute_sql_in_host(host_name, {"sql": sql}, mode)
 
         if "restore_mysql_service" in self._scenario.tags:
             params_dic = self.get_tag_params("{'restore_mysql_service'")
@@ -54,8 +101,9 @@ class RestoreEnvObject(object):
                 for k, v in mysql_vars.items():
                     query = query + "{0}={1},".format(k, v)
 
-                sql = query[:-1]#delete the last ','
+                sql = query[:-1]  # delete the last ','
                 execute_sql_in_host(mysql, {"sql": sql})
+
         if "restore_mysql_config" in self._scenario.tags:
             params_dic = self.get_tag_params("{'restore_mysql_config'")
 
@@ -69,10 +117,37 @@ class RestoreEnvObject(object):
             for host_name, mysql_vars in paras.items():
                 sed_str = ""
                 for k, v in mysql_vars.items():
-                    sed_str += "/{0}/d\n/server-id/a {0}={1}\n".format(k, v)
+                    m = ['log-bin', 'binlog_format', 'relay-log']
+                    if k in m:
+                        sed_str += "/{0}/d\n".format(k)
+                    else:
+                        sed_str += "/{0}/d\n/server-id/a {0}={1}\n".format(k, v)
 
                 mysql = ObjectFactory.create_mysql_object(host_name)
                 mysql.restart(sed_str)
+
+        if "restore_xa_recover" in self._scenario.tags:
+            params_dic = self.get_tag_params("{'restore_xa_recover'")
+
+            if params_dic:
+                paras = params_dic["restore_xa_recover"]
+            else:
+                paras = {}
+
+            logger.debug("try to restore_xa_recover of mysqls: {0}".format(paras))
+
+            for host_name in paras:
+                if host_name.find('dble') != -1:
+                    mode = "user"
+                else:
+                    mode = "mysql"
+                logger.debug("the value of host_name is: {0}, mode: {1}".format(host_name, mode))
+                sql = "xa recover"
+                res, err = execute_sql_in_host(host_name, {"sql": sql}, mode)
+                if res:
+                    for data in res:
+                        sql = "xa rollback '{0}'".format(data[3])
+                        execute_sql_in_host(host_name, {"sql": sql}, mode)
 
         if "delete_mysql_tables" in self._scenario.tags:
             params_dic = self.get_tag_params("{'delete_mysql_tables'")
@@ -84,7 +159,6 @@ class RestoreEnvObject(object):
 
             for host_name in paras.keys():
                 logger.debug("try to delete_mysql_tables of mysqls: {0}".format(host_name))
-                ssh = utils.get_ssh(host_name)
 
                 if host_name.find("dble") != -1:
                     mode = "user"
@@ -92,20 +166,12 @@ class RestoreEnvObject(object):
                     mode = "mysql"
                 logger.debug("the value of host_name is: {0}, mode: {1}".format(host_name, mode))
 
-                # in case of program exits unexpected, tables.txt Residue in /tmp
-                rc, stdout, stderr = ssh.exec_command("find /tmp -name tables.txt")
-                if len(stdout) > 0:
-                    ssh.exec_command("rm -rf /tmp/tables.txt")
-
                 for database in paras[host_name]:
-                    generate_drop_tables_sql = "select concat('drop table if exists ',table_name,';') from information_schema.TABLES where table_schema='{0}' into outfile '/tmp/tables.txt'".format(database)
-                    execute_sql_in_host(host_name, {"sql": generate_drop_tables_sql}, mode)
-
-                    # MySQLDB not support source grammar,replace with ssh cmd
-                    rc, stdout, stderr = ssh.exec_command("mysql -uroot -p111111 -D{0} -e 'source /tmp/tables.txt'".format(database))
-                    stderr = stderr.lower()
-                    assert stderr.find("error") == -1, "deletet mysql in {0}:{1} failed, err: {2}".format(host_name, database, stderr)
-                    ssh.exec_command("rm -rf /tmp/tables.txt")
+                    generate_drop_tables_sql = "select concat('drop table if exists ',table_schema,'.',table_name,';') from information_schema.TABLES where table_schema='{0}'".format(database)
+                    res, err = execute_sql_in_host(host_name, {"sql": generate_drop_tables_sql}, mode)
+                    for sql_element in res:
+                        drop_table_sql = sql_element[0]
+                        execute_sql_in_host(host_name, {"sql": drop_table_sql}, mode)
 
                 logger.debug("{0} tables has been delete success".format(host_name))
 
