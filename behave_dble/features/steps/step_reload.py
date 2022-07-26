e02e1d44fc2dcdeeb62c1f482ab3eed1f8b661b3
chenhuiming <chenhuiming@actionsky.com>
2022-07-26 10:10:01 +0800

    fix amend lost

--- behave_dble/features/steps/step_reload.py
+++ behave_dble/features/steps/step_reload.py
@@ -190,3 +190,17 @@ def get_encrypt(context, string):
 
     rc, sto, ste = context.ssh_client.exec_command(cmd)
     return sto.split('\n')[1]
+
+@Given('get config xml version from template config and named as "{var_version}"')
+def step_impl(context, var_version):
+    node = get_node("dble-1")
+    cmd_server_version = "grep '<dble:server' {0}/dble/conf/server_template.xml| grep -o 'version=\".*\"' | grep -o '[0-9]*\.[0-9]*'".format(node.install_dir)
+    cmd_schema_version = "grep '<dble:schema' {0}/dble/conf/schema_template.xml| grep -o 'version=\".*\"' | grep -o '[0-9]*\.[0-9]*'".format(node.install_dir)
+    cmd_rule_version = "grep '<dble:rule' {0}/dble/conf/rule_template.xml| grep -o 'version=\".*\"' | grep -o '[0-9]*\.[0-9]*'".format(node.install_dir)
+
+    rc1, sto1, ste1 = context.ssh_client.exec_command(cmd_server_version)
+    rc2, sto2, ste2 = context.ssh_client.exec_command(cmd_schema_version)
+    rc3, sto3, ste3 = context.ssh_client.exec_command(cmd_rule_version)
+
+    assert sto1==sto2==sto3, "versions in server_template.xml schema_template.xml rule_template.xml are not the same"
+    setattr(context, var_version, sto1)
