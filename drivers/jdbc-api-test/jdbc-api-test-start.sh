e9ef450626b54bed5713ccaed7f5f82e6279f10f
caiwei <jndxcw@163.com>
2022-07-28 13:38:03 +0800

    define DIR

--- drivers/jdbc-api-test/jdbc-api-test-start.sh
+++ drivers/jdbc-api-test/jdbc-api-test-start.sh
@@ -2,8 +2,9 @@
 # Copyright (C) 2016-2022 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
 echo '=======            package               ======='
-/usr/local/apache-maven/bin/mvn -DskipTest clean package assembly:assembly
+cd ${DIR} && /usr/local/apache-maven/bin/mvn -DskipTest clean package assembly:assembly
 echo '=======     start jdbc_api_test          ======='
-cd target && java -jar jdbc_api_test-1.0-jar-with-dependencies.jar
-echo '=======    START SUCCESS jdbc_api_test   ======='
+cd ${DIR}/target && java -jar jdbc_api_test-1.0-jar-with-dependencies.jar
