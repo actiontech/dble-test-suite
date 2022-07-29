6a7e1ef95dd230638e0cbcbfb21582b3fa1c7a00
caiwei <jndxcw@163.com>
2022-07-29 13:29:20 +0800

    format

--- drivers/jdbc-api-test/jdbc-api-test-start.sh
+++ drivers/jdbc-api-test/jdbc-api-test-start.sh
@@ -3,7 +3,6 @@
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-
 echo '=======            package               ======='
 cd ${DIR} && /usr/local/apache-maven/bin/mvn -DskipTest clean package assembly:assembly
 echo '=======     start jdbc_api_test          ======='
