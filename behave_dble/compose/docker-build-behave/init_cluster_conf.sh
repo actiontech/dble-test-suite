a375ed468a622c918cc0e8ac93a6b8868153e278
caiwei <jndxcw@163.com>
2022-08-03 18:13:55 +0800

    Update install cluster (#554)
    * improve init zk
    
    * delete unnecessary logs

--- behave_dble/compose/docker-build-behave/init_cluster_conf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#config zookeeper's myid
-dble_install=("dble-1" "dble-2" "dble-3")
-for((i=0; i<3; i=i+1)); do
-  myid=`expr ${i} + 1`
-  #docker的-t参数会为输出添加一个伪终端Allocate a pseudo-TTY ,导致ci无法捕获结果
-  docker exec -i ${dble_install[$i]}  bash -c "echo '${myid}'> /opt/zookeeper/data/myid"
-done
