793b76670808fa620427cd750a1a883a3fe6c9e8
chenhuiming <chenhuiming@actionsky.com>
2022-08-09 14:07:38 +0800

    fix driver test

--- drivers/Connector-J/run_connector_J.sh
+++ drivers/Connector-J/run_connector_J.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd ${DIR}/../../behave_dble/compose/docker-build-behave && bash resetReplication.sh
 
 echo '=======           restart dble with sql_cover_sharding               ======='
-cd ${DIR}/../../behave_dble && behave --stop -D dble_conf=sql_cover_sharding features/setup.feature
+cd ${DIR}/../../behave_dble && pipenv run behave --stop -D dble_conf=sql_cover_sharding features/setup.feature
 
 echo '=======                       package                                ======='
 cd ${DIR} && /usr/local/apache-maven/bin/mvn -DskipTest clean package assembly:assembly
@@ -17,14 +17,8 @@ cd ${DIR}/ && source do_run_connector_J.sh Jconnector-5.1.35-jar-with-dependenci
 if [ ${#res} -gt 0 ]; then
     echo "Oop! results are different with the standard ones, try 'diff -qwr ${cmp_std_res_dir}/.../file1 ${cmp_real_res_dir}/.../file2' to see the details"
     echo "${res}"
-    #save logs for ci artifacts
-    scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
-    mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
     exit 1
 else
     echo "pass"
-    #save logs for ci artifacts
-    scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
-    mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
     exit 0
 fi
