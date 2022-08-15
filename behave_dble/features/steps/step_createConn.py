92014c813c337641fa9c9b2faf88c43b40640332
caiwei <jndxcw@163.com>
2022-08-15 14:51:49 +0800

    fix ci

--- behave_dble/features/steps/step_createConn.py
+++ behave_dble/features/steps/step_createConn.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2016-2022 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 import logging
-import _thread
 import time
 import re
 from threading import Thread
