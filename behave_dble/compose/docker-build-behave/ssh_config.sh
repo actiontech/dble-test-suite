c7127ddb67c1fb61eae972de3098c2dc20eb72a2
chenhuiming <chenhuiming@actionsky.com>
2022-08-18 14:31:15 +0800

    merge docker in docker

---
+++ behave_dble/compose/docker-build-behave/ssh_config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mysql_ips=("172.100.9.1" "172.100.9.2" "172.100.9.3" "172.100.9.4" "172.100.9.5" "172.100.9.6" "172.100.9.9" "172.100.9.10" "172.100.9.11" "172.100.9.12")
+Hostname=("dble-1" "dble-2" "dble-3" "mysql" "mysql-master1" "mysql-master2" "mysql8-master1" "mysql8-master2" "mysql8-slave1" "mysql8-slave2")
+
+auto_ssh_copy_id(){
+   expect -c "set timeout -1;
+   spawn ssh-copy-id -i root@$1;
+   expect {
+    *(yes/no)* {send -- yes\r;exp_continue;}
+    *assword:* {send -- $2\r;exp_continue;}
+    eof   {exit 0;}
+   }";
+}
+
+ssh_copy_id_to_all(){
+  for((i=0; i<=9; i=i+1)); do
+   #   ssh-keygen -f "/root/.ssh/known_hosts" -R ${mysql_ips[$i]}
+   #   ssh-keygen -f "/root/.ssh/known_hosts" -R ${Hostname[$i]}
+      echo "${mysql_ips[$i]}  ${Hostname[$i]}" >> /etc/hosts 
+      auto_ssh_copy_id ${Hostname[$i]} sshpass
+  done
+}
+ssh_copy_id_to_all
--- drivers/Connector-.Net/run_connector_dotnet.sh
+++ drivers/Connector-.Net/run_connector_dotnet.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd ${DIR}/../../behave_dble/compose/docker-build-behave && bash resetReplication.sh
 
 echo '=======           restart dble with sql_cover_sharding               ======='
-cd ${DIR}/../../behave_dble && behave --stop -D dble_conf=sql_cover_sharding features/setup.feature
+cd ${DIR}/../../behave_dble && pipenv run behave --stop -D dble_conf=sql_cover_sharding features/setup.feature
 
 echo '=======                       compile                                ======='
 cd ${DIR}/netdriver && csc -out:test.exe -r:MySql.Data.dll -r:YamlDotNet.dll *.cs
@@ -17,15 +17,9 @@ cd ${DIR} && source do_run_connector_dotnet.sh -c
 if [ ${#res} -gt 0 ]; then
     echo "Oop! results are different with the standard ones, try 'diff -qwr ${cmp_std_res_dir}/.../file1 ${cmp_real_res_dir}/.../file2' to see the details"
     echo "${res}"
-     #save logs for ci artifacts
-    scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
-    mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
     exit 1
 else
     echo "pass"
-     #save logs for ci artifacts
-    scp -r root@dble-1:/opt/dble/logs ${DIR}/dble_logs
-    mv ${DIR}/sql_logs ${DIR}/dble_logs/sql_logs
     exit 0
 fi
 
