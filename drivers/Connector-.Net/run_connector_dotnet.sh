23020edb70c53941370619bcb97ed8c21c0b8968
chenhuiming <chenhuiming@actionsky.com>
2022-08-19 11:53:13 +0800

    merge docker in docker

--- drivers/Connector-.Net/run_connector_dotnet.sh
+++ drivers/Connector-.Net/run_connector_dotnet.sh
@@ -2,16 +2,17 @@
 # Copyright (C) 2016-2022 ActionTech.
 # License: https://www.mozilla.org/en-US/MPL/2.0 MPL version 2 or higher.
 
+echo '=======                 restore the running environment              ======='
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd ${DIR}/../../behave_dble/compose/docker-build-behave && bash resetReplication.sh
 
-#restart dble
-cd ${DIR}/../../behave_dble && behave --stop -D dble_conf=sql_cover_sharding features/setup.feature
+echo '=======           restart dble with sql_cover_sharding               ======='
+cd ${DIR}/../../behave_dble && pipenv run behave --stop -D dble_conf=sql_cover_sharding features/setup.feature
 
-#compile code
+echo '=======                       compile                                ======='
 cd ${DIR}/netdriver && csc -out:test.exe -r:MySql.Data.dll -r:YamlDotNet.dll *.cs
 
-#do run cpp driver test
+echo '=======                        driver test                           ======='
 cd ${DIR} && source do_run_connector_dotnet.sh -c
 if [ ${#res} -gt 0 ]; then
     echo "Oop! results are different with the standard ones, try 'diff -qwr ${cmp_std_res_dir}/.../file1 ${cmp_real_res_dir}/.../file2' to see the details"
