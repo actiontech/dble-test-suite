df3d3fa878aa167c8c3de8cb9929a89f3fa1afb4
wujinling <wujinling@actionsky.com>
2023-04-23 15:11:24 +0800

    Merge branch 'fix_java_driver' into 'master'
    将获取列名的方法由getColumnName换成getColumnLabel
    
    See merge request testing/dble-testing-backup!420
    
    (cherry picked from commit 7c02926614e96936ac3fc91a5387c489b067c442)
    
    fa2093b9 将获取列名的方法由getColumnName换成getColumnLabel

--- drivers/Connector-J/src/main/java/com/actiontech/publicFunc.java
+++ drivers/Connector-J/src/main/java/com/actiontech/publicFunc.java
@@ -22,11 +22,21 @@ public class publicFunc {
         while (rs.next()) {
             Map rowData = new HashMap();
             for (int i = 1; i <= columnCount; i++) {
-                rowData.put(md.getColumnName(i), rs.getObject(i));//get key and name
+                rowData.put(md.getColumnLabel(i), rs.getObject(i));//get key and name
             }
             list.add(rowData);
         }
         return list;
     }
+// 使用md.getColumnLabel的原因：当列有别名时getColumnLabel返回值是别名，列无别名时和getColumnName返回值相同
+// dble和mysql返回的结果是一样的，但mysql的metadata里originalColumnName无值，所以getColumnName方法返回了columnName的值，dble的metadata里originalColumnName有值，getColumnName返回的值就是originalColumnName
+// 又因hashMap是无序的，dble中将count转换成了大写，hashMap取出的值count放到了第一列
+// select pad,count(id) t from schema2.test2 group by pad having t>1;
+// dble-------------com.mysql.jdbc.ResultSetMetaData@3caeaf62 - Field level information:
+//	com.mysql.jdbc.Field@e6ea0c6[catalog=schema2,tableName=test2,originalTableName=test2,columnName=pad,originalColumnName=,mysqlType=3(FIELD_TYPE_LONG),flags=, charsetIndex=63, charsetName=US-ASCII]
+//	com.mysql.jdbc.Field@6a38e57f[catalog=schema2,tableName=test2,originalTableName=test2,columnName=t,originalColumnName=COUNT(id),mysqlType=8(FIELD_TYPE_LONGLONG),flags=, charsetIndex=63, charsetName=US-ASCII]
+// mysql============com.mysql.jdbc.ResultSetMetaData@5577140b - Field level information:
+//	com.mysql.jdbc.Field@1c6b6478[catalog=schema2,tableName=test2,originalTableName=test2,columnName=pad,originalColumnName=pad,mysqlType=3(FIELD_TYPE_LONG),flags=, charsetIndex=63, charsetName=US-ASCII]
+//	com.mysql.jdbc.Field@67f89fa3[catalog=,tableName=,originalTableName=,columnName=t,originalColumnName=,mysqlType=8(FIELD_TYPE_LONGLONG),flags=, charsetIndex=63, charsetName=US-ASCII]
 
 }
