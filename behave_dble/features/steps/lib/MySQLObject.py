e70823d55b021d0946febdec0be2e1516122a7db
wujinling <wujinling@actionsky.com>
2023-05-31 11:03:24 +0800

    Merge branch 'kill_conn_8.0' into 'master'
    mysql8.0 event_scheduler参数默认开启，会在show processlist结果中显示，kill connection时排除event_scheduler
    
    See merge request testing/dble-testing-backup!471
    
    (cherry picked from commit 80657963855b205344d0891ececf0850cec8c913)
    
    dda67080 mysql8.0 event_scheduler参数默认开启，会在show processlist结果中显示，kill connection时排除event_scheduler

--- behave_dble/features/steps/lib/MySQLObject.py
+++ behave_dble/features/steps/lib/MySQLObject.py
@@ -255,7 +255,8 @@ class MySQLObject(object):
         for row in res:
             conn_id = row[0]
             not_show_processlist = row[7] != "show processlist"#for excluding show processlist conn itself
-            if not_show_processlist and (exclude_conn_ids is None or str(conn_id) not in exclude_conn_ids):
+            not_event_scheduler = row[1] != "event_scheduler"#for excluding event_scheduler, mysql8.0 event_scheduler is on
+            if not_event_scheduler and not_show_processlist and (exclude_conn_ids is None or str(conn_id) not in exclude_conn_ids):
                 res, err = conn.execute("kill {}".format(conn_id))
                 assert err is None, "kill conn '{}' failed for {}".format(conn_id,err[1])
         logger.debug("kill connections success, excluding connection ids:{}".format(exclude_conn_ids))
@@ -270,7 +271,8 @@ class MySQLObject(object):
         for row in res:
             conn_id = row[0]
             not_show_processlist = row[7] != "show processlist"#for excluding show processlist conn itself
-            if not_show_processlist and str(conn_id) in conn_ids:
+            not_event_scheduler = row[1] != "event_scheduler"  # for excluding event_scheduler, mysql8.0 event_scheduler is on
+            if not_event_scheduler and not_show_processlist and str(conn_id) in conn_ids:
                 res, err = conn.execute("kill {}".format(conn_id))
                 assert err is None, "kill conn '{}' failed for {}".format(conn_id,err[1])
         logger.debug("kill connections success, killed connection ids:{}".format(conn_ids))
@@ -290,7 +292,8 @@ class MySQLObject(object):
         for row in res:
             conn_id = row[0]
             not_show_processlist = row[7] != "show processlist"#for excluding show processlist conn itself
-            if not_show_processlist and conn_id in conn_ids:
+            not_event_scheduler = row[1] != "event_scheduler"#for excluding event_scheduler, mysql8.0 event_scheduler is on
+            if not_event_scheduler and not_show_processlist and conn_id in conn_ids:
                 res, err = conn.execute("kill {}".format(conn_id))
                 assert err is None, "kill conn '{}' failed for {}".format(conn_id,err[1])
                 logger.debug("kill connection id {} success".format(conn_id))
